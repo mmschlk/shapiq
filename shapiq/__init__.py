b1aea55c4b1d817a87d116b1d6aa5c3e6b48e0e1
Maximilian <maximilian.muschalik@gmail.com>
2023-10-17 17:34:49 +0200

    Create __init__.py

---
+++ shapiq/__init__.py
@@ -0,0 +1,5 @@
+"""shapiq is a library creating explanations for machine learning models based on
+the well established Shapley value and its generalization to interaction.
+"""
+
+from .__version__ import __version__
