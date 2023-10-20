eecbfb11b3d9ee845567d487808a7dadcd4a44a7
Maximilian <maximilian.muschalik@gmail.com>
2023-10-20 15:12:32 +0200

    updated docs

--- shapiq/utils/game_theory.py
+++ shapiq/utils/game_theory.py
@@ -16,6 +16,14 @@ def powerset(
 ) -> Iterable[Any]:
     """Return a powerset of an iterable as tuples with optional size limits.
 
+    Args:
+        iterable: Iterable.
+        min_size: Minimum size of the subsets. Defaults to 0 (start with the empty set).
+        max_size: Maximum size of the subsets. Defaults to None (all possible sizes).
+
+    Returns:
+        iterable: Powerset of the iterable.
+
     Example:
         >>> list(powerset([1, 2, 3]))
         [(), (1,), (2,), (3,), (1, 2), (1, 3), (2, 3), (1, 2, 3)]
@@ -25,14 +33,6 @@ def powerset(
 
         >>> list(powerset([1, 2, 3], max_size=2))
         [(), (1,), (2,), (3,), (1, 2), (1, 3), (2, 3)]
-
-    Args:
-        iterable: Iterable.
-        min_size: Minimum size of the subsets. Defaults to 0 (start with the empty set).
-        max_size: Maximum size of the subsets. Defaults to None (all possible sizes).
-
-    Returns:
-        iterable: Powerset of the iterable.
     """
     s = list(iterable)
     max_size = len(s) if max_size is None else min(max_size, len(s))
