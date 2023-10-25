4004b62d8132b932748598fc0518f6a12fa4aba8
Maximilian <maximilian.muschalik@gmail.com>
2023-10-25 18:17:17 +0200

    changed imports

--- shapiq/approximator/__init__.py
+++ shapiq/approximator/__init__.py
@@ -1,1 +1,8 @@
 """This module contains the approximators to estimate the Shapley interaction values."""
+from permutation.sii import PermutationSamplingSII
+from permutation.sti import PermutationSamplingSTI
+
+__all__ = [
+    "PermutationSamplingSII",
+    "PermutationSamplingSTI"
+]
---
+++ shapiq/approximator/permutation/__init__.py
@@ -0,0 +1,6 @@
+"""This module contains all permutation-based sampling algorithms to estimate SII/nSII and STI."""
+from _base import PermutationSampling
+
+__all__ = [
+    "PermutationSampling"
+]
