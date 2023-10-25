87ebf4cb5853b596ab61582aa84223539e61a8f9
Maximilian <maximilian.muschalik@gmail.com>
2023-10-25 18:15:38 +0200

    updated types

--- shapiq/approximator/_base.py
+++ shapiq/approximator/_base.py
@@ -1,8 +1,8 @@
 """This module contains the base approximator classes for the shapiq package."""
-
+import copy
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Callable, Union
+from typing import Callable, Union, Optional
 
 import numpy as np
 
@@ -64,7 +64,14 @@ class Approximator(ABC):
             to max_order. Otherwise, min_order is equal to 1.
     """
 
-    def __init__(self, n: int, max_order: int, index: str, top_order: bool) -> None:
+    def __init__(
+            self,
+            n: int,
+            max_order: int,
+            index: str,
+            top_order: bool,
+            random_state: Optional[int] = None
+    ) -> None:
         """Initializes the approximator."""
         self.index: str = index
         if self.index not in AVAILABLE_INDICES:
@@ -77,6 +84,8 @@ class Approximator(ABC):
         self.max_order: int = max_order
         self.top_order: bool = top_order
         self.min_order: int = self.max_order if self.top_order else 1
+        self._random_state: Optional[int] = random_state
+        self._rng: Optional[np.random.Generator] = None
 
     @abstractmethod
     def approximate(
@@ -101,7 +110,8 @@ class Approximator(ABC):
         raise NotImplementedError
 
     def _init_result(self, dtype=float) -> dict[int, np.ndarray]:
-        """Initializes the result dictionary.
+        """Initializes the result dictionary mapping from order to the interaction values.
+        For order 1 the interaction values are of shape (n,) for order 2 of shape (n, n) and so on.
 
         Args:
             dtype: The data type of the result dictionary values. Defaults to float.
@@ -109,9 +119,32 @@ class Approximator(ABC):
         Returns:
             The result dictionary.
         """
-        result = {s: np.zeros(self.n + 1, dtype=dtype)
-                  for s in range(self.min_order, self.max_order + 1)}
+        result = {s: self._get_empty_array(self.n, s, dtype=dtype)
+                  for s in self._order_iterator}
         return result
+
+    @staticmethod
+    def _get_empty_array(n: int, order: int, dtype=float) -> np.ndarray:
+        """Returns an empty array of the appropriate shape for the given order.
+
+        Args:
+            n: The number of players.
+            order: The order of the array.
+            dtype: The data type of the array. Defaults to float.
+
+        Returns:
+            The empty array.
+        """
+        return np.zeros(n ** order, dtype=dtype).reshape((n,) * order)
+
+    @property
+    def _order_iterator(self) -> range:
+        """Returns an iterator over the orders of the approximation.
+
+        Returns:
+            The iterator.
+        """
+        return range(self.min_order, self.max_order + 1)
 
     def _finalize_result(self, result) -> InteractionValues:
         """Finalizes the result dictionary.
@@ -123,3 +156,26 @@ class Approximator(ABC):
             The interaction values.
         """
         return InteractionValues(result, self.index, self.max_order)
+
+    @staticmethod
+    def _smooth_with_epsilon(
+            interaction_results: Union[dict, np.ndarray],
+            eps=0.00001
+    ) -> Union[dict, np.ndarray]:
+        """Smooth the interaction results with a small epsilon to avoid numerical issues.
+
+        Args:
+            interaction_results: Interaction results.
+            eps: Small epsilon. Defaults to 0.00001.
+
+        Returns:
+            Union[dict, np.ndarray]: Smoothed interaction results.
+        """
+        if not isinstance(interaction_results, dict):
+            interaction_results[np.abs(interaction_results) < eps] = 0
+            return copy.deepcopy(interaction_results)
+        interactions = {}
+        for interaction_order, interaction_values in interaction_results.items():
+            interaction_values[np.abs(interaction_values) < eps] = 0
+            interactions[interaction_order] = interaction_values
+        return copy.deepcopy(interactions)
