229af5b00b72a5816928107313ebb10dbffe55e1
Maximilian <maximilian.muschalik@gmail.com>
2024-01-05 14:36:40 +0100

    started work on the stacked bar plots for nSII

--- shapiq/plot/_config.py
+++ shapiq/plot/_config.py
@@ -1,12 +1,27 @@
 """This module contains the configuration for the shapiq visualizations."""
 from colour import Color
 
-RED = Color("#ff0d57")
-BLUE = Color("#1e88e5")
-NEUTRAL = Color("#ffffff")
-
 __all__ = [
     "RED",
     "BLUE",
     "NEUTRAL",
+    "COLORS_N_SII",
 ]
+
+RED = Color("#ff0d57")
+BLUE = Color("#1e88e5")
+NEUTRAL = Color("#ffffff")
+
+COLORS_N_SII = [
+    "#D81B60",
+    "#FFB000",
+    "#1E88E5",
+    "#FE6100",
+    "#7F975F",
+    "#74ced2",
+    "#708090",
+    "#9966CC",
+    "#CCCCCC",
+    "#800080",
+]
+COLORS_N_SII = COLORS_N_SII * (100 + (len(COLORS_N_SII)))  # repeat the colors list
