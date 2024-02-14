9a22dda024f59624fe772658b5916801f24de89d
Maximilian <maximilian.muschalik@gmail.com>
2024-02-14 09:55:10 +0100

    adds a collaboration tutorial and code of conduct (closes #39).

--- shapiq/approximator/__init__.py
+++ shapiq/approximator/__init__.py
@@ -1,4 +1,5 @@
 """This module contains the approximators to estimate the Shapley interaction values."""
+from .k_sii import convert_ksii_into_one_dimension, transforms_sii_to_ksii
 from .permutation.sii import PermutationSamplingSII
 from .permutation.sti import PermutationSamplingSTI
 from .regression import KernelSHAP, RegressionFSI, RegressionSII
@@ -11,4 +12,6 @@ __all__ = [
     "RegressionFSI",
     "RegressionSII",
     "ShapIQ",
+    "transforms_sii_to_ksii",
+    "convert_ksii_into_one_dimension",
 ]
