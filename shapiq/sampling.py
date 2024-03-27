9fab01f5cc9a54a8e5cb76065309adefde62cc71
FFmgll <ffumagalli@techfak.uni-bielefeld.de>
2024-03-27 09:11:01 +0100

    current working state

--- shapiq/sampling.py
+++ shapiq/sampling.py
@@ -1,15 +1,19 @@
 """This module contains stochastic sampling procedures for coalitions of players."""
+import copy
+
 import numpy as np
 from typing import Optional
 from scipy.special import binom
 from utils import powerset
+import warnings
+from collections import Counter
 
-class CoalitionSampler():
+class CoalitionSampler:
     """The coalition sampler to generate a collection of subsets as a basis for approximation methods.
 
     Args:
         n: Number of elements to sample from
-        sampling_weights: Sampling for weights for coalition sizes.
+        sampling_weights: Sampling for weights for coalition sizes, must be non-negative and at least one >0.
         random_state: The random state to use for the sampling process. Defaults to `None`.
 
     Attributes:
@@ -18,51 +22,65 @@ class CoalitionSampler():
         random_state: The random state to use for the sampling process. Defaults to `None`.
 
     Example:
-        >>> from games import DummyGame
-        >>> from approximator import ShapIQ
-        >>> game = DummyGame(n=5, interaction=(1, 2))
-        >>> approximator = ShapIQ(n=5, max_order=2, index="SII")
-        >>> approximator.approximate(budget=50, game=game)
-        InteractionValues(
-            index=SII, order=2, estimated=False, estimation_budget=32,
-            values={
-                (0,): 0.2,
-                (1,): 0.7,
-                (2,): 0.7,
-                (3,): 0.2,
-                (4,): 0.2,
-                (0, 1): 0,
-                (0, 2): 0,
-                (0, 3): 0,
-                (0, 4): 0,
-                (1, 2): 1.0,
-                (1, 3): 0,
-                (1, 4): 0,
-                (2, 3): 0,
-                (2, 4): 0,
-                (3, 4): 0
-            }
-        )
     """
 
     def __init__(
         self,
          n: np.ndarray,
          sampling_weights: np.ndarray,
-         border_trick: bool = False,
          pairing_trick: bool = False,
          random_state: Optional[int] = None
     ) -> None:
         self.n = n
+        self.N = np.array(range(n))
         self.sampling_weights = sampling_weights/np.sum(sampling_weights)
-        self.border_trick = border_trick
         self.pairing_trick = pairing_trick
-        self.random_state = random_state
-        assert n+1==np.size(sampling_weights), "n elements corresponds to n+1 subset sizes (including empty subsets)"
+        self._random_state = random_state
+        self._rng: Optional[np.random.Generator] = np.random.default_rng(seed=self._random_state)
+        self.coalitions_to_exclude = []
+        self.coalitions_to_compute = []
+        self.coalitions_to_sample = list(range(self.n+1))
+        #Set maximum of possible coalitions by excluding zero-weighted coalition sizes
+        # Coalition sizes with zero weights, should be removed and excluded from total number of coalitions
+        self.n_max_coalitions = 2 ** self.n
+        #Handle zero-weighted coalition sizes
+        for size, weight in enumerate(self.sampling_weights):
+            if weight == 0:
+                self.n_max_coalitions -= int(binom(self.n, size))
+                self.coalitions_to_exclude.extend([self.coalitions_to_sample.pop(self.coalitions_to_sample.index(size))])
+        self.adjusted_sampling_weights = self.sampling_weights[self.coalitions_to_sample]/np.sum(self.sampling_weights[self.coalitions_to_sample])
+        #Check shape of sampling weights for coalition sizes 0,...,n
+        assert n+1==np.size(sampling_weights), "n elements must correspond to n+1 coalition sizes (including empty subsets)"
+        #Check non-negativity of sampling weights
+        assert (sampling_weights>=0).all(),"All sampling weights must be non-negative"
 
-    def sample_coalitions_from_budget(
+    def get_coalitions_matrix(self):
+        return copy.deepcopy(self.sampled_coalitions_matrix)
+
+    def get_coalitions_counter(self):
+        return copy.deepcopy(self.sampled_coself.coalitions_to_samplealitions_counter)
+
+    def get_coalitions_probabilities(self):
+        return copy.deepcopy(self.sampled_coalitions_probs)
+
+    def _execute_border_trick(self,sampling_budget):
+        coalitions_per_size = np.array([binom(self.n, k) for k in range(self.n + 1)])
+        expected_number_of_coalitions = sampling_budget * self.adjusted_sampling_weights
+        sampling_exceeds_expectation = expected_number_of_coalitions >= coalitions_per_size[self.coalitions_to_sample]
+        while sampling_exceeds_expectation.any():
+            coalitions_to_move = [self.coalitions_to_sample[index] for index, include in enumerate(sampling_exceeds_expectation) if include]
+            self.coalitions_to_compute.extend(
+                [self.coalitions_to_sample.pop(self.coalitions_to_sample.index(move_this)) for move_this in coalitions_to_move])
+            sampling_budget -= int(np.sum(coalitions_per_size[coalitions_to_move]))
+            self.adjusted_sampling_weights = self.adjusted_sampling_weights[~sampling_exceeds_expectation] / np.sum(
+                self.adjusted_sampling_weights[~sampling_exceeds_expectation])
+            expected_number_of_coalitions = sampling_budget * self.adjusted_sampling_weights
+            sampling_exceeds_expectation = expected_number_of_coalitions >= coalitions_per_size[self.coalitions_to_sample]
+        return sampling_budget
+
+    def sample(
     self,
-    budget:int
+    sampling_budget:int
     ) -> np.ndarray:
         """Samples distinct coalitions according to the specified budget.
 
@@ -71,83 +89,101 @@ class CoalitionSampler():
 
         Returns:
         A binary matrix of coalitions normalized with probabilities and number of Monte Carlo samples.
+        A dictionary of parameters of the sampling procedures, including:
+        - the number of occurrences of each coalition
+        - the probability of a coalition to be included in the collection
+        - a list indicating if the coalitions were sampled or computed explicitly (the latter is only applicable, if border_trick = True)
+        - the number of coalitions for each coalition size
         """
 
-        sampling_budget = budget
-        adjusted_sampling_weights = self.sampling_weights/np.sum(self.sampling_weights)
-        sampled_coalitions_counter_dict = {}
-        sampled_coalitions_counter = np.zeros((sampling_budget))
-        sampled_coalitions_matrix = np.zeros((sampling_budget, self.n), dtype=int)
-        sampled_coalitions_weights = np.zeros((sampling_budget),dtype=float)
-        coalitions_to_compute = []
-        coalitions_to_sample = list(range(self.n+1))
+        self.sampled_coalitions_dict = {}
 
-        if self.border_trick:
-            coalitions_per_size = np.array([binom(self.n,k) for k in range(n+1)])
-            expected_number_of_coalitions = budget * self.sampling_weights
-            sampling_exceeds_expectation = expected_number_of_coalitions>=coalitions_per_size
-            while sampling_exceeds_expectation.any():
-                coalitions_to_move = [index for index, include in enumerate(sampling_exceeds_expectation) if include]
-                coalitions_to_compute.extend([coalitions_to_sample.pop(index) for index in sorted(coalitions_to_move,reverse=True)])
-                sampling_budget -= np.sum(coalitions_per_size[coalitions_to_move])
-                adjusted_sampling_weights = adjusted_sampling_weights[~sampling_exceeds_expectation]/np.sum(adjusted_sampling_weights[~sampling_exceeds_expectation])
-                expected_number_of_coalitions = sampling_budget*adjusted_sampling_weights
-                sampling_exceeds_expectation = expected_number_of_coalitions >= coalitions_per_size[coalitions_to_sample]
-                if len(coalitions_to_sample) == 0 and sampling_budget > 0:
-                    sampled_coalitions_matrix = np.zeros((2**self.n, self.n), dtype=int)
-                    sampled_coalitions_counter = np.zeros(2**self.n)
-                    sampled_coalitions_weights = np.zeros(2**self.n)
 
-                    Warning("Not all budget might be needed due to the border-trick")
-                    break
+        if sampling_budget >= self.n_max_coalitions:
+            warnings.warn("Not all budget is required due to the border-trick.", UserWarning)
 
-        coalitions_to_sample.sort()
-        coalitions_to_compute.sort()
-        if len(coalitions_to_sample) > 0:
+        #Adjust sampling budget to max coalitions
+        sampling_budget = min(sampling_budget,self.n_max_coalitions)
+
+        #Variables to be computed
+        self.coalitions_per_size = np.zeros(self.n+1,dtype=int)
+        self.sampled_coalitions_counter = np.zeros((sampling_budget))
+        self.sampled_coalitions_matrix = np.zeros((sampling_budget, self.n), dtype=int)
+        self.sampled_coalitions_probs = np.zeros((sampling_budget), dtype=float)
+        self.is_coalition_size_sampled = np.zeros(self.n+1,dtype=bool)
+        permutation = np.arange(self.n)
+
+        #Border-Trick: Enumerate all coalitions, where the expected number of coalitions exceeds the total number
+        sampling_budget = self._execute_border_trick(sampling_budget)
+
+        #Sort by size for esthetics
+        self.coalitions_to_sample.sort()
+        self.coalitions_to_compute.sort()
+        if len(self.coalitions_to_sample) > 0: #Sample, if there are coalition sizes to sample from
+            #Draw random coalition sizes based on adjusted_sampling_weights
+            coalition_sizes = self._rng.choice(self.coalitions_to_sample, p=self.adjusted_sampling_weights,size=sampling_budget)
+            initial_budget = len(coalition_sizes)
+            counter = 0
             #Start sampling procedure
             while sampling_budget > 0:
-                coalition_size = np.random.choice(coalitions_to_sample, p=adjusted_sampling_weights)
-                coalition_indices = np.random.choice(self.n, size=coalition_size, replace=True)
+                #generate random permutation
+                self._rng.shuffle(permutation)
+                coalition_size = coalition_sizes[counter]
+                counter += 1
+                counter %= initial_budget
+                coalition_indices = permutation[:coalition_size]
                 coalition_indices.sort()  # Sorting for consistent representation
                 coalition_tuple = tuple(coalition_indices)
-                if sampled_coalitions_counter_dict.get(coalition_tuple):
+                self.coalitions_per_size[coalition_size] += 1
+                if self.sampled_coalitions_dict.get(coalition_tuple):
                     #if coalition is not new
-                    sampled_coalitions_counter_dict[coalition_tuple] += 1
+                    self.sampled_coalitions_dict[coalition_tuple] += 1
                 else:
-                    sampled_coalitions_counter_dict[coalition_tuple] = 1
+                    self.sampled_coalitions_dict[coalition_tuple] = 1
                     sampling_budget -= 1
                 if self.pairing_trick and sampling_budget > 0:
                     paired_coalition_size = self.n-coalition_size
-                    if paired_coalition_size not in coalitions_to_sample:
-                        Warning("Pairing is affected as weights are not symmetric and border-trick is enabled.")
+                    if paired_coalition_size not in self.coalitions_to_sample:
+                        warnings.warn("Pairing is affected as weights are not symmetric and border-trick is enabled.",UserWarning)
                     else:
-                        paired_coalition_tuple = tuple(set(range(self.n))-set(coalition_tuple))
-                        if sampled_coalitions_counter_dict.get(paired_coalition_tuple):
+                        paired_coalition_indices = permutation[coalition_size:]
+                        paired_coalition_indices.sort()
+                        paired_coalition_tuple = tuple(paired_coalition_indices)
+                        self.coalitions_per_size[paired_coalition_size] += 1
+                        if self.sampled_coalitions_dict.get(paired_coalition_tuple):
                             #if coalition is not new
-                            sampled_coalitions_counter_dict[paired_coalition_tuple] += 1
+                            self.sampled_coalitions_dict[paired_coalition_tuple] += 1
                         else:
-                            sampled_coalitions_counter_dict[paired_coalition_tuple] = 1
+                            self.sampled_coalitions_dict[paired_coalition_tuple] = 1
                             sampling_budget -= 1
 
 
         # Convert coalition counts to a binary matrix
         coalition_index = 0
-        for coalition_size in coalitions_to_compute:
+        for coalition_size in self.coalitions_to_compute:
+            self.coalitions_per_size[coalition_size] = int(binom(self.n,coalition_size))
             for T in powerset(range(n),min_size=coalition_size,max_size=coalition_size):
-                sampled_coalitions_matrix[coalition_index, list(T)] = 1
-                sampled_coalitions_counter[coalition_index] = 1
-                sampled_coalitions_weights[coalition_index] = 1
-                coalition_index+=1
-        for coalition_tuple, count in sampled_coalitions_counter.items():
-            sampled_coalitions_matrix[coalition_index, list(coalition_tuple)] = 1
-            sampled_coalitions_counter[coalition_index] = count
-            sampled_coalitions_weights[coalition_index] = 1
+                self.sampled_coalitions_matrix[coalition_index, list(T)] = 1
+                self.sampled_coalitions_counter[coalition_index] = 1
+                self.sampled_coalitions_probs[coalition_index] = 1
+                coalition_index += 1
+        for coalition_tuple, count in self.sampled_coalitions_dict.items():
+            self.sampled_coalitions_matrix[coalition_index, list(coalition_tuple)] = 1
+            self.sampled_coalitions_counter[coalition_index] = count
+            self.sampled_coalitions_probs[coalition_index] = self.adjusted_sampling_weights[self.coalitions_to_sample.index(len(coalition_tuple))]/self.coalitions_per_size[len(coalition_tuple)]
             coalition_index += 1
 
-        return sampled_coalitions_matrix
+        for coalition_size in self.coalitions_to_sample:
+            self.is_coalition_size_sampled[coalition_size] = True
 
 
 if __name__ == "__main__":
     n = 10
-    sampler = CoalitionSampler(n,np.ones(n+1),border_trick=True,pairing_trick=True)
-    result = sampler.sample_coalitions_from_budget(500)
+    sampling_weights = np.zeros(n+1)
+    sampling_weights[4:6] = 1
+    #sampling_weights[1] = -1
+    #sampling_weights[4:6] *= 1000000
+    sampler = CoalitionSampler(n,sampling_weights,pairing_trick=False,random_state=43)
+    sampler.sample(5000)
+    coalitions_matrix = sampler.get_coalitions_matrix()
+    print("ok")
---
+++ tests/test_sampling.py
@@ -0,0 +1,120 @@
+"""This test module contains all tests regarding the sampling class"""
+from copy import copy, deepcopy
+
+import numpy as np
+import pytest
+from games import DummyGame
+import CoalitionSampler
+
+
+@pytest.mark.parametrize(
+    "n, sampling_weights, border_trick, pairing_trick, random_state",
+    [
+        (10, np.ones(11), False),
+        (10, np.ones(11), True),
+        (10,np.ones(11)-2,True), #expected to fail
+        (10,np.ones(10),False) #expected to fail
+    ],
+)
+def test_initialization(n, sampling_weights, pairing_trick):
+    """Tests the initialization of the CoalitionSampler."""
+    assert approximator.n == n
+    assert approximator.max_order == max_order
+    assert approximator.top_order is top_order
+    assert approximator.min_order == (max_order if top_order else 1)
+    assert approximator.iteration_cost == 1
+    assert approximator.index == index
+
+    approximator_copy = copy(approximator)
+    approximator_deepcopy = deepcopy(approximator)
+    approximator_deepcopy.index = "something"
+    assert approximator_copy == approximator  # check that the copy is equal
+    assert approximator_deepcopy != approximator  # check that the deepcopy is not equal
+    approximator_string = str(approximator)
+    assert repr(approximator) == approximator_string
+    assert hash(approximator) == hash(approximator_copy)
+    assert hash(approximator) != hash(approximator_deepcopy)
+    with pytest.raises(ValueError):
+        _ = approximator == 1
+    with pytest.raises(ValueError):
+        approximator_deepcopy._weight_kernel(3, 2)
+
+
+@pytest.mark.parametrize("n, max_order, budget, batch_size", [(7, 2, 100, None), (7, 2, 100, 10)])
+def test_approximate_fsi(n, max_order, budget, batch_size):
+    """Tests the approximation of the ShapIQ FSI approximation."""
+    interaction = (1, 2)
+    game = DummyGame(n, interaction)
+    approximator = ShapIQ(n, max_order, index="FSI", top_order=True, random_state=42)
+    estimates = approximator.approximate(budget, game, batch_size=batch_size)
+    assert isinstance(estimates, InteractionValues)
+    assert estimates.max_order == max_order
+    assert estimates.min_order == max_order  # only top order for FSI
+
+    # check that the budget is respected
+    assert game.access_counter <= budget + 2
+
+    # for order 2 (max_order) the interaction between player 1 and 2 is the most important (1.0)
+    interaction_estimate = estimates[interaction]
+    assert interaction_estimate == pytest.approx(1.0, 0.4)  # large tolerance for FSI
+
+
+@pytest.mark.parametrize(
+    "n, max_order, top_order, budget, batch_size",
+    [(7, 2, False, 100, None), (7, 2, True, 100, 10), (7, 2, False, 300, None)],
+)
+def test_approximate_sii(n, max_order, top_order, budget, batch_size):
+    """Tests the approximation of the ShapIQ SII approximation."""
+    interaction = (1, 2)
+    game = DummyGame(n, interaction)
+    approximator = ShapIQ(n, max_order, index="SII", top_order=top_order, random_state=42)
+    estimates = approximator.approximate(budget, game, batch_size=batch_size)
+    assert isinstance(estimates, InteractionValues)
+    assert estimates.max_order == max_order
+    assert estimates.min_order == (max_order if top_order else 1)
+
+    # check that the budget is respected
+    assert game.access_counter <= budget + 2
+
+    # for order 2 (max_order) the interaction between player 1 and 2 is the most important (1.0)
+    assert estimates[interaction] == pytest.approx(1.0, 0.4)
+
+    if not top_order:
+        # for order 1 (min_order) the interaction between  1 and 2 is the most important (0.6429)
+        if budget <= 2**n:
+            assert estimates[(1,)] == pytest.approx(0.6429, 0.4)
+            assert estimates[(2,)] == pytest.approx(0.6429, 0.4)
+        else:
+            assert estimates[(1,)] == pytest.approx(0.6429, 0.0001)
+            assert estimates[(2,)] == pytest.approx(0.6429, 0.0001)
+
+        # check efficiency
+        assert np.sum(estimates.values[:n]) == pytest.approx(2.0, 0.4)
+
+
+@pytest.mark.parametrize(
+    "n, max_order, top_order, budget, batch_size", [(7, 2, False, 100, None), (7, 2, True, 100, 10)]
+)
+def test_approximate_sti(n, max_order, top_order, budget, batch_size):
+    """Tests the approximation of the ShapIQ STI approximation."""
+    interaction = (1, 2)
+    game = DummyGame(n, interaction)
+    approximator = ShapIQ(n, max_order, index="STI", top_order=top_order, random_state=42)
+    estimates = approximator.approximate(budget, game, batch_size=batch_size)
+    assert isinstance(estimates, InteractionValues)
+    assert estimates.max_order == max_order
+    assert estimates.min_order == (max_order if top_order else 1)
+
+    # check that the budget is respected
+    assert game.access_counter <= budget + 2
+
+    # for order 2 (max_order) the interaction between player 1 and 2 is the most important (1.0)
+    assert estimates[interaction] == pytest.approx(1.0, 0.5)
+
+    if not top_order:
+        # for order 1 (min_order) the interaction between player 1 and 2 is the most important (0.7)
+        assert estimates[(1,)] == pytest.approx(1 / 7, 0.4)
+        assert estimates[(2,)] == pytest.approx(1 / 7, 0.4)
+
+        # check efficiency
+        assert np.sum(estimates.values) == pytest.approx(2.0, 0.4)
