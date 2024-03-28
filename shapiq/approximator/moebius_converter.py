7a8011396301044da05f646284a32d3aa016604a
FFmgll <ffumagalli@techfak.uni-bielefeld.de>
2024-03-28 22:00:16 +0100

    added kSII and unified all interaction conversion

--- shapiq/approximator/moebius_converter.py
+++ shapiq/approximator/moebius_converter.py
@@ -1,12 +1,11 @@
-from scipy.special import bernoulli, binom
-import numpy as np
-
 # from shapiq import powerset
-
 # TODO: Remove this part when import works
 from collections.abc import Iterable
 from itertools import chain, combinations
 from typing import Any, Optional
+
+import numpy as np
+from scipy.special import bernoulli, binom
 
 
 def powerset(
@@ -42,123 +41,151 @@ class MoebiusConverter:
             n_interactions[i] = n_interaction
         return n_interactions
 
-    def convert_to_interactions(self, index, order):
+    # TODO: remove base-aggregation when formula for k-SII is fixed
+    def base_aggregation(self, base_values, order):
+        transformed = {}
+        # Initialize emptyset baseline value
+        # Lookup Bernoulli numbers
+        bernoulli_numbers = bernoulli(order)
+
+        for S in powerset(set(range(self.n)), min_size=1, max_size=order):
+            subset_size = len(S)
+            if S in base_values:
+                S_effect = base_values[S]
+            else:
+                S_effect = 0
+            # go over all subsets S_tilde of length |S| + 1, ..., n that contain S
+            for S_tilde in powerset(set(range(self.n)), min_size=subset_size + 1, max_size=order):
+                if not set(S).issubset(S_tilde):
+                    continue
+                # get the effect of T
+                if S_tilde in base_values:
+                    S_tilde_effect = base_values[S_tilde]
+                else:
+                    S_tilde_effect = 0
+                # normalization with bernoulli numbers
+                S_effect += bernoulli_numbers[len(S_tilde) - subset_size] * S_tilde_effect
+            transformed[S] = S_effect
+        return transformed
+
+    def _get_moebius_distribution_weight(
+        self, moebius_size: int, interaction_size: int, order: int, index: str
+    ):
+        if index == "SII":
+            return 1 / (moebius_size - interaction_size + 1)
+        if index == "STII":
+            if moebius_size <= order:
+                return 1
+            else:
+                if interaction_size == order:
+                    return 1 / binom(moebius_size, moebius_size - interaction_size)
+                else:
+                    return 0
+        if index == "FSII":
+            if moebius_size <= order:
+                return 1
+            else:
+                return (
+                    (-1) ** (order - interaction_size)
+                    * (interaction_size / (order + interaction_size))
+                    * (
+                        binom(order, interaction_size)
+                        * binom(moebius_size - 1, order)
+                        / binom(moebius_size + order - 1, order + interaction_size)
+                    )
+                )
+        if index == "k-SII":
+            if moebius_size <= order:
+                return 1
+            else:
+                rslt = 0
+                bernoulli_numbers = bernoulli(order)
+                for k in range(order - interaction_size + 1):
+                    rslt += (
+                        binom(order - interaction_size, k)
+                        * bernoulli_numbers[k]
+                        / (1 + moebius_size - interaction_size - k)
+                    )
+                return rslt
+
+    def moebius_to_interaction(self, order, index):
         """
         Converts the Möbius coefficients to Shapley Interactions up to order k
         """
-        if index == "SII":
-            shapley_interactions = self.moebius_to_sii(order)
         if index == "k-SII":
-            shapley_interactions = self.moebius_to_ksii(order)
+            tmp_index = "k-SII"
+            index = "SII"
+        else:
+            tmp_index = None
+
+        rslt = {}
+        # Set lowest interaction size that receives higher-order Möbius values, default is 1, for STII is order
         if index == "STII":
-            shapley_interactions = self.moebius_to_stii(order)
-        if index == "FSII":
-            shapley_interactions = self.moebius_to_fsii(order)
-        return shapley_interactions
+            # STII distributes only to interactions of maximum order
+            min_receiving_interaction = order
+        else:
+            # Distributes value to all interactions
+            min_receiving_interaction = 1
 
-    def moebius_to_sii(self, order):
-        rslt = {}
-        for moebius_set, moebius_val in self.moebius_coefficients.items():
-            # Distribute the value among all contained interactions
-            moebius_size = len(moebius_set)
-            for interaction in powerset(
-                moebius_set, min_size=1, max_size=int(min(moebius_size, order))
-            ):
-                interaction_size = len(interaction)
-                if interaction in rslt:
-                    rslt[interaction] += moebius_val / (moebius_size - interaction_size + 1)
-                else:
-                    rslt[interaction] = moebius_val / (moebius_size - interaction_size + 1)
-        return rslt
+        # Set indices with no distribution, if Möbius set <= order
+        if index in ["FSII", "STII", "k-SII"]:
+            trivial_moebius_interactions = True
+        else:
+            # SII still distributes this value
+            trivial_moebius_interactions = False
 
-    def moebius_to_ksii(self, order):
-        rslt = {}
+        # Pre-compute weights
+        distribution_weights = np.zeros((self.n + 1, order + 1))
+        for moebius_size in range(1, self.n + 1):
+            for interaction_size in range(1, min(order, moebius_size) + 1):
+                distribution_weights[
+                    moebius_size, interaction_size
+                ] = self._get_moebius_distribution_weight(
+                    moebius_size, interaction_size, order, index
+                )
+
         for moebius_set, moebius_val in self.moebius_coefficients.items():
             moebius_size = len(moebius_set)
-            # For lower-order Möbius sets (size<= order) directly set interaction value
-            if moebius_size <= order:
+            # For lower-order Möbius sets (size<= order) and indices with trivial_low_interactions directly set interaction value
+            if moebius_size <= order and trivial_moebius_interactions:
+                val_distributed = distribution_weights[moebius_size, moebius_size]
                 if moebius_set in rslt:
-                    rslt[moebius_set] += moebius_val
+                    rslt[moebius_set] += moebius_val * val_distributed
                 else:
-                    rslt[moebius_set] = moebius_val
+                    rslt[moebius_set] = moebius_val * val_distributed
             else:
                 # For higher-order Möbius sets (size > order) distribute the value among all contained interactions
-                for interaction in powerset(moebius_set, min_size=order, max_size=order):
-                    val_for_interaction = moebius_val / binom(moebius_size, moebius_size - order)
+                for interaction in powerset(
+                    moebius_set, min_size=min_receiving_interaction, max_size=order
+                ):
+                    val_distributed = distribution_weights[moebius_size, len(interaction)]
+                    # Check if Möbius value is distributed onto this interaction
                     if interaction in rslt:
-                        rslt[interaction] += val_for_interaction
+                        rslt[interaction] += moebius_val * val_distributed
                     else:
-                        rslt[interaction] = val_for_interaction
-        return rslt
+                        rslt[interaction] = moebius_val * val_distributed
 
-    def moebius_to_stii(self, order):
-        rslt = {}
-        for moebius_set, moebius_val in self.moebius_coefficients.items():
-            moebius_size = len(moebius_set)
-            # For lower-order Möbius sets (size<= order) directly set interaction value
-            if moebius_size <= order:
-                if moebius_set in rslt:
-                    rslt[moebius_set] += moebius_val
-                else:
-                    rslt[moebius_set] = moebius_val
-            else:
-                # For higher-order Möbius sets (size > order) distribute the value among all contained interactions
-                for interaction in powerset(moebius_set, min_size=order, max_size=order):
-                    val_for_interaction = moebius_val / binom(moebius_size, moebius_size - order)
-                    if interaction in rslt:
-                        rslt[interaction] += val_for_interaction
-                    else:
-                        rslt[interaction] = val_for_interaction
-        return rslt
-
-    def moebius_to_fsii(self, order):
-        rslt = {}
-        for moebius_set, moebius_val in self.moebius_coefficients.items():
-            moebius_size = len(moebius_set)
-            if moebius_size <= order:
-                # For lower-order Möbius sets (size<= order) directly set interaction value
-                if moebius_set in rslt:
-                    rslt[moebius_set] += moebius_val
-                else:
-                    rslt[moebius_set] = moebius_val
-            else:
-                # For higher-order Möbius sets (size > order) distribute the value among all contained interactions
-                for interaction in powerset(moebius_set, min_size=1, max_size=order):
-                    interaction_size = len(interaction)
-                    val_for_interaction = (
-                        moebius_val
-                        * (-1) ** (order - interaction_size)
-                        * (interaction_size / (order + interaction_size))
-                        * (
-                            binom(order, interaction_size)
-                            * binom(moebius_size - 1, order)
-                            / binom(moebius_size + order - 1, order + interaction_size)
-                        )
-                    )
-                    if interaction in rslt:
-                        rslt[interaction] += val_for_interaction
-                    else:
-                        rslt[interaction] = val_for_interaction
-        return rslt
+        if tmp_index is None:
+            return rslt
+        else:
+            return self.base_aggregation(rslt, order)
 
 
 if __name__ == "__main__":
-    n = 5
+    n = 10
+    """
     moebius_converter = MoebiusConverter(n, soum.moebius_coefficients_dict)
     order = 2
-    sii = moebius_converter.moebius_to_sii(order)
-    rslt = 0
-    for set, val in sii.items():
-        if len(set) == 1:
-            rslt += val
-    print("SII: ", rslt)
-    stii = moebius_converter.moebius_to_stii(order)
-    rslt = 0
-    for set, val in stii.items():
-        rslt += val
-    print("STII: ", rslt)
-    fsii = moebius_converter.moebius_to_fsii(order)
-    rslt = 0
-    for set, val in fsii.items():
-        rslt += val
-    print("FSII: ", rslt)
+
+    values = {}
+    for index in ["SII", "STII", "k-SII", "FSII"]:
+        values[index] = moebius_converter.moebius_to_interaction(order, index)
+        rslt = 0
+        for interaction, value in values[index].items():
+            if index == "SII":
+                if len(interaction) == 1:
+                    rslt += value
+            else:
+                rslt += value
+        print(index, ": ", rslt)
+    """
