80aa67518e4d6a966f8b09cfa49fab719824a876
Maximilian <maximilian.muschalik@gmail.com>
2024-04-09 16:54:42 +0200

    replaced repo bike and california data openml

--- shapiq/datasets/_all.py
+++ shapiq/datasets/_all.py
@@ -1,43 +1,6 @@
 """This module contains functions to load datasets."""
 
-import os
-
 import pandas as pd
-
-GITHUB_DATA_URL = "https://github.com/mmschlk/shapiq/raw/main/data/"
-
-
-def load_bike() -> tuple[pd.DataFrame, pd.Series]:
-    """Load the bike-sharing dataset from a Kaggle competition.
-
-    Original source: https://www.kaggle.com/c/bike-sharing-demand
-
-    Note:
-        The function and preprocessing is taken from the `sage` package.
-
-    Returns:
-        The bike-sharing dataset as a pandas DataFrame.
-    """
-    data = pd.read_csv(os.path.join(GITHUB_DATA_URL, "bike.csv"))
-    columns = data.columns.tolist()
-
-    # Split and remove datetime column.
-    data["datetime"] = pd.to_datetime(data["datetime"])
-    data["year"] = data["datetime"].dt.year
-    data["month"] = data["datetime"].dt.month
-    data["day"] = data["datetime"].dt.day
-    data["hour"] = data["datetime"].dt.hour
-    data = data.drop("datetime", axis=1)
-
-    # Reorder and rename columns.
-    data = data[["year", "month", "day", "hour"] + columns[1:]]
-    data.columns = list(map(str.title, data.columns))
-
-    # get the target column
-    x_data = data.drop(columns=["Count"])
-    y_data = data["Count"]
-
-    return x_data, y_data
 
 
 def _get_open_ml_dataset(open_ml_id, version=1):
@@ -59,6 +22,55 @@ def _get_open_ml_dataset(open_ml_id, version=1):
     class_label = dataset.default_target_attribute
     x_data = dataset.get_data()[0]
     return x_data, class_label
+
+
+def load_bike() -> tuple[pd.DataFrame, pd.Series]:
+    """Load the bike-sharing dataset from openml.
+
+    Note:
+        The function requires the `openml` and `sklearn` packages to be installed.
+
+    Returns:
+        The bike-sharing dataset as a pandas DataFrame.
+    """
+    from sklearn.compose import ColumnTransformer
+    from sklearn.pipeline import Pipeline
+    from sklearn.preprocessing import OrdinalEncoder, RobustScaler
+
+    dataset, class_label = _get_open_ml_dataset(42713, version=1)
+    num_feature_names = ["hour", "temp", "feel_temp", "humidity", "windspeed"]
+    cat_feature_names = [
+        "season",
+        "year",
+        "month",
+        "holiday",
+        "weekday",
+        "workingday",
+        "weather",
+    ]
+    dataset[num_feature_names] = dataset[num_feature_names].apply(pd.to_numeric)
+    num_pipeline = Pipeline([("scaler", RobustScaler())])
+    cat_pipeline = Pipeline(
+        [
+            ("ordinal_encoder", OrdinalEncoder()),
+        ]
+    )
+    column_transformer = ColumnTransformer(
+        [
+            ("numerical", num_pipeline, num_feature_names),
+            ("categorical", cat_pipeline, cat_feature_names),
+        ],
+        remainder="passthrough",
+    )
+    col_names = num_feature_names + cat_feature_names
+    col_names += [feature for feature in dataset.columns if feature not in col_names]
+    dataset = pd.DataFrame(column_transformer.fit_transform(dataset), columns=col_names)
+    dataset.dropna(inplace=True)
+
+    x_data = dataset
+    y_data = dataset.pop(class_label)
+
+    return x_data, y_data
 
 
 def load_adult_census() -> tuple[pd.DataFrame, pd.Series]:
