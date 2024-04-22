629fc1a2b4a7b145588b387cc9edb3af4964e191
Maximilian <maximilian.muschalik@gmail.com>
2024-04-22 09:47:36 +0200

    fixed import order

--- shapiq/approximator/__init__.py
+++ shapiq/approximator/__init__.py
@@ -1,7 +1,7 @@
 """This module contains the approximators to estimate the Shapley interaction values."""
 
 from .k_sii import convert_ksii_into_one_dimension, transforms_sii_to_ksii
-from .marginals import StratifiedSamplingSV, OwenSamplingSV
+from .marginals import OwenSamplingSV, StratifiedSamplingSV
 from .permutation.sii import PermutationSamplingSII
 from .permutation.stii import PermutationSamplingSTII
 from .permutation.sv import PermutationSamplingSV
