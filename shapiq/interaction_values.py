0dee853e37799a502799fca5cecc17764318ed19
Maximilian <maximilian.muschalik@gmail.com>
2024-05-23 16:25:39 +0200

    bugfix in data loading

--- shapiq/interaction_values.py
+++ shapiq/interaction_values.py
@@ -506,18 +506,18 @@ class InteractionValues:
         """
         # try loading as npz file
         try:
-            with np.load(path, allow_pickle=True) as data:
-                return InteractionValues(
-                    values=data["values"],
-                    index=str(data["index"]),
-                    max_order=int(data["max_order"]),
-                    n_players=int(data["n_players"]),
-                    min_order=int(data["min_order"]),
-                    interaction_lookup=data["interaction_lookup"].item(),
-                    estimated=bool(data["estimated"]),
-                    estimation_budget=data["estimation_budget"].item(),
-                    baseline_value=float(data["baseline_value"]),
-                )
+            data = np.load(path, allow_pickle=True)
+            return InteractionValues(
+                values=data["values"],
+                index=str(data["index"]),
+                max_order=int(data["max_order"]),
+                n_players=int(data["n_players"]),
+                min_order=int(data["min_order"]),
+                interaction_lookup=data["interaction_lookup"].item(),
+                estimated=bool(data["estimated"]),
+                estimation_budget=data["estimation_budget"].item(),
+                baseline_value=float(data["baseline_value"]),
+            )
         except AttributeError:  # not a npz file
             pass
         with open(path, "rb") as file:
