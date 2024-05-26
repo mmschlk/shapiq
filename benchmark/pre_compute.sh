d13f568776beec262b27206251a569ca1949e024
Maximilian <maximilian.muschalik@gmail.com>
2024-05-26 23:26:21 +0200

    changed setting

--- benchmark/pre_compute.sh
+++ benchmark/pre_compute.sh
@@ -8,19 +8,24 @@
 # name of the python file to run
 PYTHON_FILE="pre_compute.py"
 
-# script settings: PYTHON_FILE <game_name> <config_id>
+# script settings: PYTHON_FILE <game_name> <config_id> <n_player_id>
 
-# run the python file
-python $PYTHON_FILE --game AdultCensusUnsupervisedData --config_id 1 &
-python $PYTHON_FILE --game AdultCensusClusterExplanation --config_id 1 &
-python $PYTHON_FILE --game AdultCensusClusterExplanation --config_id 2 &
-python $PYTHON_FILE --game BikeSharingClusterExplanation --config_id 1 &
-python $PYTHON_FILE --game BikeSharingClusterExplanation --config_id 2 &
-python $PYTHON_FILE --game CaliforniaHousingClusterExplanation --config_id 1 &
-python $PYTHON_FILE --game CaliforniaHousingClusterExplanation --config_id 2 &
-python $PYTHON_FILE --game AdultCensusEnsembleSelection --config_id 1 &
-python $PYTHON_FILE --game BikeSharingEnsembleSelection --config_id 1 &
-python $PYTHON_FILE --game CaliforniaHousingEnsembleSelection --config_id 1 &
-python $PYTHON_FILE --game AdultCensusRandomForestEnsembleSelection --config_id 1 &
-python $PYTHON_FILE --game BikeSharingRandomForestEnsembleSelection --config_id 1 &
-python $PYTHON_FILE --game CaliforniaHousingRandomForestEnsembleSelection --config_id 1 &
+# run the python file with nohup to run in the background
+nohup python $PYTHON_FILE --game AdultCensusRandomForestEnsembleSelection --config_id 1 &
+nohup python $PYTHON_FILE --game BikeSharingRandomForestEnsembleSelection --config_id 1 &
+nohup python $PYTHON_FILE --game CaliforniaHousingRandomForestEnsembleSelection --config_id 1 &
+nohup python $PYTHON_FILE --game ImageClassifierLocalXAI --config_id 1 --n_player_id 2 &
+nohup python $PYTHON_FILE --game ImageClassifierLocalXAI --config_id 1 --n_player_id 3 &
+nohup python $PYTHON_FILE --game SentimentAnalysisLocalXAI --config_id 1 --n_player_id 1 &
+nohup python $PYTHON_FILE --game AdultCensusFeatureSelection --config_id 1 &
+nohup python $PYTHON_FILE --game AdultCensusFeatureSelection --config_id 2 &
+nohup python $PYTHON_FILE --game AdultCensusFeatureSelection --config_id 3 &
+nohup python $PYTHON_FILE --game BikeSharingFeatureSelection --config_id 1 &
+nohup python $PYTHON_FILE --game BikeSharingFeatureSelection --config_id 2 &
+nohup python $PYTHON_FILE --game BikeSharingFeatureSelection --config_id 3 &
+nohup python $PYTHON_FILE --game CaliforniaHousingFeatureSelection --config_id 1 &
+nohup python $PYTHON_FILE --game CaliforniaHousingFeatureSelection --config_id 2 &
+nohup python $PYTHON_FILE --game CaliforniaHousingFeatureSelection --config_id 3 &
+nohup python $PYTHON_FILE --game AdultCensusEnsembleSelection --config_id 1 &
+nohup python $PYTHON_FILE --game BikeSharingEnsembleSelection --config_id 1 &
+nohup python $PYTHON_FILE --game CaliforniaHousingEnsembleSelection --config_id 1 &
