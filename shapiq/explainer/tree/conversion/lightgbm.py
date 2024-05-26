cbbbdf29657504564406409177c8308cc2d6f5d4
Hubert Baniecki <hbaniecki@gmail.com>
2024-05-26 19:30:05 +0200

    add multiclass support for lightgbm; closes #54

--- shapiq/explainer/tree/conversion/lightgbm.py
+++ shapiq/explainer/tree/conversion/lightgbm.py
@@ -13,72 +13,71 @@ from ..base import TreeModel
 
 
 def convert_lightgbm_booster(
-    tree_model: Model,
+    tree_booster: Model,
     class_label: Optional[int] = None,
 ) -> list[TreeModel]:
-    """Transforms a lightgbm model to the format used by shapiq.
+    """Transforms models from the ``lightgbm`` package to the format used by ``shapiq``.
 
     Args:
-        tree_model: The lightgbm booster to convert.
-        class_label: The class label of the model to explain. Only used for classification models.
-            Defaults to 0.
+        tree_booster: The lightgbm booster to convert.
+        class_label: The class label of the model to explain. Only used for multiclass 
+            classification models. Defaults to 0.
 
     Returns:
-        The converted random forest model.
+        The converted lightgbm booster.
     """
-    scaling = 1.0 / tree_model.num_trees()
-    df_booster = tree_model.trees_to_dataframe()
-    convert_feature_str_to_int = {k: v for v, k in enumerate(tree_model.feature_name())}
+    scaling = 1.0 / tree_booster.num_trees()
+    booster_df = tree_booster.trees_to_dataframe()
+    if tree_booster.params['objective'] in ['binary', 'multiclass']:
+        # convert raw to probabilities
+        booster_df['value'] = _sigmoid(booster_df['value'])
+    if tree_booster.params['objective'] == "multiclass":
+        # choose only trees for the selected class (lightgbm grows n_estimators*n_class trees)
+        if class_label is None:
+            class_label = 0
+        idc = booster_df['tree_index'] % tree_booster.num_model_per_iteration() == class_label
+        booster_df = booster_df[idc]
+    convert_feature_str_to_int = {k: v for v, k in enumerate(tree_booster.feature_name())}
     # pandas can't chill https://stackoverflow.com/q/77900971
     with pd.option_context('future.no_silent_downcasting', True):
-        df_booster['split_feature'] = df_booster['split_feature']\
+        booster_df['split_feature'] = booster_df['split_feature']\
             .replace(convert_feature_str_to_int).infer_objects(copy=False)
     return [
-        convert_lightgbm_tree(tree, scaling=scaling, class_label=class_label)
-        for i, tree in df_booster.groupby("tree_index")
+        _convert_lightgbm_tree_as_df(tree_df, scaling=scaling)
+        for i, tree_df in booster_df.groupby("tree_index")
     ]
 
 
-def convert_lightgbm_tree(
-    tree_model: Model, class_label: Optional[int] = None, scaling: float = 1.0
+def _convert_lightgbm_tree_as_df(
+    tree_df: Model, scaling: float = 1.0
 ) -> TreeModel:
     """Convert a lightgbm decision tree to the format used by shapiq.
 
     Args:
-        tree_model: The lightgbm decision tree model to convert.
-        class_label: The class label of the model to explain. Only used for classification models.
-            Defaults to 0.
+        tree_df: The lightgbm decision tree model formatted as a data frame.
         scaling: The scaling factor for the tree values.
 
     Returns:
         The converted decision tree model.
     """
     output_type = "raw"
-    # tree_values = tree_model.tree_.value.copy() * scaling
-    # set class label if not given and model is a classifier
-    # if safe_isinstance(tree_model, "sklearn.tree.DecisionTreeClassifier") and class_label is None:
-    #     class_label = 1
-
-    # if class_label is not None:
-    #     # turn node values into probabilities
-    #     if len(tree_values.shape) == 3:
-    #         tree_values = tree_values[:, 0, :]
-    #     tree_values = tree_values / np.sum(tree_values, axis=1, keepdims=True)
-    #     tree_values = tree_values[:, class_label]
-    #     output_type = "probability"
-    convert_node_str_to_int = {k: v for v, k in enumerate(tree_model.node_index)}
+    convert_node_str_to_int = {k: v for v, k in enumerate(tree_df.node_index)}
 
     # pandas can't chill https://stackoverflow.com/q/77900971
     with pd.option_context('future.no_silent_downcasting', True):
         return TreeModel(
-            children_left=tree_model['left_child']\
+            children_left=tree_df['left_child']\
                 .replace(convert_node_str_to_int).infer_objects(copy=False).fillna(-1).astype(int).values,
-            children_right=tree_model['right_child']\
+            children_right=tree_df['right_child']\
                 .replace(convert_node_str_to_int).infer_objects(copy=False).fillna(-1).astype(int).values,
-            features=tree_model['split_feature'].fillna(-2).astype(int).values,
-            thresholds=tree_model['threshold'].values,
-            values=tree_model['value'].values,
-            node_sample_weight=tree_model['count'].values,
+            features=tree_df['split_feature'].fillna(-2).astype(int).values,
+            thresholds=tree_df['threshold'].values,
+            values=tree_df['value'].values * scaling,
+            node_sample_weight=tree_df['count'].values,
             empty_prediction=None,  # compute empty prediction later
-            original_output_type=output_type,
+            original_output_type=output_type, # not used
         )
+    
+
+def _sigmoid(x):
+    return 1/(1+np.exp(-x)) 
