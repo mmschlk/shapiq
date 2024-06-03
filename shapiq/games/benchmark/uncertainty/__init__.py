47c5e5b0be4f424ace6503b65378d4b7394e2772
Paul Hofman <paulwhofman@hotmail.com>
2024-06-03 17:06:56 +0200

    Added uncertainty game and benchmark

---
+++ shapiq/games/benchmark/uncertainty/__init__.py
@@ -0,0 +1,7 @@
+"""This module contains all uncertainty explanation benchmark games."""
+
+from .benchmark import AdultCensus
+
+__all__ = ["AdultCensus"]
+
+# Path: shapiq/games/benchmark/uncertainty/__init__.py
