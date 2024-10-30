320518ae3a70f8f0e6c50ad1d54994adef10f4a4
Maximilian <maximilian.muschalik@gmail.com>
2024-10-30 10:44:47 +0100

    Adds `BaselineImputer` and Reworks `MarginalImputer` (#262)
    * started work on #107
    
    * refactores imputer tests
    
    * reworks imputers
    
    * reworks imputers
    
    * finishes baseline imputer and adds joint-marginal-dist. to MarginalImputer
    
    * adds test to joint marginal prediciton and closes #261
    
    * updated documentation

--- shapiq/games/__init__.py
+++ shapiq/games/__init__.py
@@ -1,9 +1,9 @@
 """Game objects for the shapiq package."""
 
-# from . import benchmark
+# from . import benchmark  # not imported here to avoid circular imports and long import times
 from .base import Game
-from .imputer import ConditionalImputer, MarginalImputer
+from .imputer import BaselineImputer, ConditionalImputer, MarginalImputer
 
-__all__ = ["Game", "MarginalImputer", "ConditionalImputer"]  # + benchmark.__all__
+__all__ = ["Game", "MarginalImputer", "ConditionalImputer", "BaselineImputer"]
 
 # Path: shapiq/games/__init__.py
