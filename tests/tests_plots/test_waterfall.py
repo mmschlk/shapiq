85ba7faf7a2e2d8ac134a2ab5db8b26c0c166a99
Thies, Santo <s.thies@campus.lmu.de>
2025-01-05 13:16:31 +0100

    Add concrete test for waterfall

--- tests/tests_plots/test_waterfall.py
+++ tests/tests_plots/test_waterfall.py
@@ -7,6 +7,54 @@ from shapiq.interaction_values import InteractionValues
 from shapiq.plot import waterfall_plot
 
 
+def test_waterfall_concrete():
+    import numpy as np
+
+    import shapiq
+
+    class CookingGame(shapiq.Game):
+        def __init__(self):
+            self.characteristic_function = {
+                (): 10,
+                (0,): 4,
+                (1,): 3,
+                (2,): 2,
+                (0, 1): 9,
+                (0, 2): 8,
+                (1, 2): 7,
+                (0, 1, 2): 15,
+            }
+            super().__init__(
+                n_players=3,
+                player_names=["Alice", "Bob", "Charlie"],  # Optional list of names
+                normalization_value=self.characteristic_function[()],  # 0
+                normalize=False,
+            )
+
+        def value_function(self, coalitions: np.ndarray) -> np.ndarray:
+            """Defines the worth of a coalition as a lookup in the characteristic function."""
+            output = []
+            for coalition in coalitions:
+                output.append(self.characteristic_function[tuple(np.where(coalition)[0])])
+            return np.array(output)
+
+    cooking_game = CookingGame()
+
+    from shapiq import ExactComputer
+
+    # create an ExactComputer object for the cooking game
+    exact_computer = ExactComputer(n_players=cooking_game.n_players, game_fun=cooking_game)
+
+    # compute the Shapley Values for the game
+    sv_exact = exact_computer(index="k-SII", order=2)
+    print(sv_exact.dict_values)
+
+    # visualize the Shapley Values
+    from shapiq.plot import waterfall_plot
+
+    waterfall_plot(sv_exact, show=True)
+
+
 def test_waterfall_plot(interaction_values_list: list[InteractionValues]):
     """Test the waterfall plot function."""
     iv = interaction_values_list[0]
