Adds trivial TreeExplainer computation and fixes a Bug in parsing xgboost models. (#334)

shapiq/explainer/tree/conversion/xgboost.py
@@ -3,8 +3,6 @@ shapiq."""
 
 import warnings
 
-import numpy as np
-
 from ....utils.types import Model
 from ..base import TreeModel
 
@@ -42,10 +40,8 @@ def convert_xgboost_booster(
 
     if tree_booster.feature_names:
         feature_names = tree_booster.feature_names
-    else:
-        # xgboost does not store default feature names
-        n_features = len(np.setdiff1d(np.unique(booster_df["Feature"]), "Leaf"))
-        feature_names = [f"f{i}" for i in range(n_features)]
+    else:  # xgboost does not store default feature names
+        feature_names = [f"f{i}" for i in range(tree_booster.num_features())]
     convert_feature_str_to_int = {k: v for v, k in enumerate(feature_names)}
     convert_feature_str_to_int["Leaf"] = -2
     booster_df.loc[:, "Feature"] = booster_df.loc[:, "Feature"].replace(convert_feature_str_to_int)

tests/fixtures/data.py
@@ -1,5 +1,13 @@
-"""This fixtures module contains data fixtures for the tests."""
+"""This fixtures module contains data fixtures for the tests.
 
+Note to developers:
+    This module should only contain fixtures containing data. If you need to creat a model fixture,
+    please use the `models.py` module and import the data fixtures from this module. Further, always
+    use the `copy.deepcopy` function to ensure that the fixtures are not modified during the tests.
+    This is especially important for the data fixtures, as they are used in multiple tests.
+"""
+
+import copy
 import os
 
 import numpy as np
@@ -7,7 +15,13 @@ import pytest
 from PIL import Image
 from sklearn.datasets import make_classification, make_regression
 
-NR_FEATURES = 7  # Number of features for the tabular models
+# normal datasets
+NR_FEATURES = 7
+NR_FEATURES_INFORMATIVE = 7
+
+# small datasets
+NR_FEATURES_SMALL = 3
+NR_FEATURES_SMALL_INFORMATIVE = 2
 
 
 @pytest.fixture
@@ -21,36 +35,21 @@ def if_clf_dataset() -> tuple[np.ndarray, np.ndarray]:
     outliers = rng.uniform(low=-4, high=4, size=(n_outliers, 2))
     X = np.concatenate([cluster_1, cluster_2, outliers])
     y = np.concatenate([np.ones((2 * n_samples), dtype=int), -np.ones((n_outliers), dtype=int)])
-    return X, y
-
-
-@pytest.fixture
-def background_reg_data() -> np.ndarray:
-    """Return a simple background dataset."""
-    X, _ = make_regression(n_samples=100, n_features=7, random_state=42)
-    return X
-
-
-@pytest.fixture
-def background_clf_data() -> np.ndarray:
-    """Return a simple background dataset."""
-    X, _ = make_classification(
-        n_samples=100,
-        n_features=NR_FEATURES,
-        random_state=42,
-        n_classes=3,
-        n_informative=7,
-        n_repeated=0,
-        n_redundant=0,
-    )
-    return X
+    return copy.deepcopy(X), copy.deepcopy(y)
 
 
 @pytest.fixture
 def background_reg_dataset() -> tuple[np.ndarray, np.ndarray]:
     """Return a simple background dataset."""
     X, y = make_regression(n_samples=100, n_features=NR_FEATURES, random_state=42)
-    return X, y
+    return copy.deepcopy(X), copy.deepcopy(y)
+
+
+@pytest.fixture
+def background_reg_data(background_reg_dataset) -> np.ndarray:
+    """Return a simple background dataset."""
+    X, _ = background_reg_dataset
+    return copy.deepcopy(X)
 
 
 @pytest.fixture
@@ -61,11 +60,18 @@ def background_clf_dataset() -> tuple[np.ndarray, np.ndarray]:
         n_features=NR_FEATURES,
         random_state=42,
         n_classes=3,
-        n_informative=7,
+        n_informative=NR_FEATURES_INFORMATIVE,
         n_repeated=0,
         n_redundant=0,
     )
-    return X, y
+    return copy.deepcopy(X), copy.deepcopy(y)
+
+
+@pytest.fixture
+def background_clf_data(background_clf_dataset) -> np.ndarray:
+    """Return a simple background dataset."""
+    X, _ = background_clf_dataset
+    return copy.deepcopy(X)
 
 
 @pytest.fixture
@@ -76,11 +82,33 @@ def background_clf_dataset_binary() -> tuple[np.ndarray, np.ndarray]:
         n_features=NR_FEATURES,
         random_state=42,
         n_classes=2,
-        n_informative=7,
+        n_informative=NR_FEATURES_INFORMATIVE,
         n_repeated=0,
         n_redundant=0,
     )
-    return X, y
+    return copy.deepcopy(X), copy.deepcopy(y)
+
+
+@pytest.fixture
+def background_clf_dataset_binary_small() -> tuple[np.ndarray, np.ndarray]:
+    """Return a simple background dataset."""
+    X, y = make_classification(
+        n_samples=10,
+        n_features=NR_FEATURES_SMALL,
+        random_state=42,
+        n_classes=2,
+        n_informative=NR_FEATURES_SMALL_INFORMATIVE,
+        n_repeated=0,
+        n_redundant=NR_FEATURES_SMALL - NR_FEATURES_SMALL_INFORMATIVE,
+    )
+    return copy.deepcopy(X), copy.deepcopy(y)
+
+
+@pytest.fixture
+def background_reg_dataset_small() -> tuple[np.ndarray, np.ndarray]:
+    """Return a simple background dataset."""
+    X, y = make_regression(n_samples=10, n_features=NR_FEATURES_SMALL, random_state=42)
+    return copy.deepcopy(X), copy.deepcopy(y)
 
 
 @pytest.fixture
@@ -91,4 +119,4 @@ def image_and_path() -> tuple[Image.Image, str]:
         os.path.dirname(os.path.abspath(__file__)), "..", "data", "test_croc.JPEG"
     )
     image = Image.open(path_from_test_root)
-    return image, path_from_test_root
+    return copy.deepcopy(image), copy.deepcopy(path_from_test_root)

tests/fixtures/models.py
@@ -1,14 +1,14 @@
 """This fixtures module contains model fixtures for the tests."""
 
-from typing import Any
-
 import numpy as np
 import pytest
-from sklearn.datasets import make_classification, make_regression
 from sklearn.ensemble import IsolationForest, RandomForestClassifier, RandomForestRegressor
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+
+from shapiq.explainer.tree import TreeModel
+from shapiq.utils import Model
 
 NR_FEATURES = 7  # Number of features for the tabular models
 
@@ -30,9 +30,10 @@ TABULAR_TORCH_MODEL_FIXTURES = [
 ]
 
 TREE_MODEL_FIXTURES = [
+    ("xgb_reg_model", "xgboost.sklearn.XGBRegressor"),
+    ("xgb_clf_model", "xgboost.sklearn.XGBClassifier"),
     ("lightgbm_reg_model", "lightgbm.sklearn.LGBMRegressor"),
-    ("xgboost_regressor", "xgboost.sklearn.XGBRegressor"),
-    ("xgboost_booster", "xgboost.core.Booster"),
+    ("lightgbm_clf_model", "lightgbm.sklearn.LGBMClassifier"),
     ("lightgbm_basic", "lightgbm.basic.Booster"),
     ("rf_reg_model", "sklearn.ensemble.RandomForestRegressor"),
     ("rf_clf_model", "sklearn.ensemble.RandomForestClassifier"),
@@ -56,30 +57,7 @@ def custom_model(background_reg_dataset) -> CustomModel:
 
 
 @pytest.fixture
-def xgboost_regressor(background_reg_dataset):
-    """Return a xgb regression model"""
-    import xgboost as xgb
-
-    X, y = background_reg_dataset
-    model = xgb.XGBRegressor(n_estimators=10, max_depth=1)
-    model.fit(X, y, verbose=False)
-    return model
-
-
-@pytest.fixture
-def xgboost_booster(background_reg_dataset):
-    """Return a xgb booster"""
-    import xgboost as xgb
-
-    X, y = background_reg_dataset
-    dtrain = xgb.DMatrix(X, label=y)
-    params = {"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7}
-    booster = xgb.train(params=params, dtrain=dtrain, num_boost_round=0)
-    return booster
-
-
-@pytest.fixture
-def lightgbm_basic(background_reg_dataset):
+def lightgbm_basic(background_reg_dataset) -> Model:
     """Return a lgm basic booster"""
     import lightgbm as lgb
 
@@ -90,24 +68,24 @@ def lightgbm_basic(background_reg_dataset):
 
 
 @pytest.fixture
-def sequential_model_1_class():
+def sequential_model_1_class() -> Model:
     """Return a keras nn with output dimension 1"""
     return _sequential_model(1)
 
 
 @pytest.fixture
-def sequential_model_2_classes():
+def sequential_model_2_classes() -> Model:
     """Return a keras nn with output dimension 2"""
     return _sequential_model(2)
 
 
 @pytest.fixture
-def sequential_model_3_classes():
+def sequential_model_3_classes() -> Model:
     """Return a keras nn with output dimension 3"""
     return _sequential_model(3)
 
 
-def _sequential_model(output_shape_nr):
+def _sequential_model(output_shape_nr, background_reg_dataset) -> Model:
     """Return a keras nn with specified output dimension"""
     import keras
 
@@ -119,60 +97,44 @@ def _sequential_model(output_shape_nr):
         ]
     )
     model.compile(optimizer="adam", loss="mse")
-    X, y = make_regression(n_samples=100, n_features=NR_FEATURES, random_state=42)
+    X, y = background_reg_dataset
     model.fit(X, y, epochs=0, batch_size=32)
     return model
 
 
 @pytest.fixture
-def xgb_reg_model():
+def xgb_reg_model(background_reg_dataset) -> Model:
     """Return a simple xgboost regression model."""
     from xgboost import XGBRegressor
 
-    X, y = make_regression(n_samples=100, n_features=7, random_state=42)
+    X, y = background_reg_dataset
     model = XGBRegressor(random_state=42, n_estimators=3)
     model.fit(X, y)
     return model
 
 
 @pytest.fixture
-def rf_clf_binary_model() -> RandomForestClassifier:
+def rf_clf_binary_model(background_clf_dataset_binary) -> RandomForestClassifier:
     """Return a simple random forest model."""
-    X, y = make_classification(
-        n_samples=100,
-        n_features=NR_FEATURES,
-        random_state=42,
-        n_classes=2,
-        n_informative=7,
-        n_repeated=0,
-        n_redundant=0,
-    )
+    X, y = background_clf_dataset_binary
     model = RandomForestClassifier(random_state=42, max_depth=3, n_estimators=3)
     model.fit(X, y)
     return model
 
 
 @pytest.fixture
-def xgb_clf_model():
+def xgb_clf_model(background_clf_dataset) -> Model:
     """Return a simple xgboost classification model."""
     from xgboost import XGBClassifier
 
-    X, y = make_classification(
-        n_samples=100,
-        n_features=NR_FEATURES,
-        random_state=42,
-        n_classes=3,
-        n_informative=7,
-        n_repeated=0,
-        n_redundant=0,
-    )
+    X, y = background_clf_dataset
     model = XGBClassifier(random_state=42, n_estimators=3)
     model.fit(X, y)
     return model
 
 
 @pytest.fixture
-def torch_clf_model():
+def torch_clf_model() -> Model:
     """Return a simple torch model."""
     import torch
 
@@ -186,7 +148,7 @@ def torch_clf_model():
 
 
 @pytest.fixture
-def torch_reg_model():
+def torch_reg_model() -> Model:
     """Return a simple torch model."""
     import torch
 
@@ -200,11 +162,13 @@ def torch_reg_model():
 
 
 @pytest.fixture
-def tabpfn_classification_problem() -> tuple[Any, np.ndarray, np.ndarray, np.ndarray]:
+def tabpfn_classification_problem(
+    background_clf_dataset_binary_small,
+) -> tuple[Model, np.ndarray, np.ndarray, np.ndarray]:
     """Returns a very simple tabpfn classifier and dataset."""
     from tabpfn import TabPFNClassifier
 
-    data, labels = make_classification(n_samples=10, n_features=3, random_state=42, n_redundant=1)
+    data, labels = background_clf_dataset_binary_small
     data, x_test, labels, _ = train_test_split(data, labels, random_state=42, train_size=8)
     model = TabPFNClassifier()
     model.fit(data, labels)
@@ -212,11 +176,13 @@ def tabpfn_classification_problem() -> tuple[Any, np.ndarray, np.ndarray, np.nda
 
 
 @pytest.fixture
-def tabpfn_regression_problem() -> tuple[Any, np.ndarray, np.ndarray, np.ndarray]:
+def tabpfn_regression_problem(
+    background_reg_dataset_small,
+) -> tuple[Model, np.ndarray, np.ndarray, np.ndarray]:
     """Returns a very simple tabpfn regressor and dataset."""
     from tabpfn import TabPFNRegressor
 
-    data, labels = make_regression(n_samples=10, n_features=3, random_state=42)
+    data, labels = background_reg_dataset_small
     data, x_test, labels, _ = train_test_split(data, labels, random_state=42, train_size=8)
     model = TabPFNRegressor()
     model.fit(data, labels)
@@ -224,101 +190,69 @@ def tabpfn_regression_problem() -> tuple[Any, np.ndarray, np.ndarray, np.ndarray
 
 
 @pytest.fixture
-def dt_reg_model() -> DecisionTreeRegressor:
+def dt_reg_model(background_reg_dataset) -> DecisionTreeRegressor:
     """Return a simple decision tree model."""
-    X, y = make_regression(n_samples=100, n_features=NR_FEATURES, random_state=42)
+    X, y = background_reg_dataset
     model = DecisionTreeRegressor(random_state=42, max_depth=3)
     model.fit(X, y)
     return model
 
 
 @pytest.fixture
-def dt_clf_model() -> DecisionTreeClassifier:
+def dt_clf_model(background_clf_dataset) -> DecisionTreeClassifier:
     """Return a simple decision tree model."""
-    X, y = make_classification(
-        n_samples=100,
-        n_features=NR_FEATURES,
-        random_state=42,
-        n_classes=3,
-        n_informative=7,
-        n_repeated=0,
-        n_redundant=0,
-    )
+    X, y = background_clf_dataset
     model = DecisionTreeClassifier(random_state=42, max_depth=3)
     model.fit(X, y)
     return model
 
 
 @pytest.fixture
-def lr_clf_model() -> LogisticRegression:
+def lr_clf_model(background_clf_dataset) -> LogisticRegression:
     """Return a simple logistic regression model."""
-    X, y = make_classification(
-        n_samples=100,
-        n_features=7,
-        random_state=42,
-        n_classes=3,
-        n_informative=7,
-        n_repeated=0,
-        n_redundant=0,
-    )
+    X, y = background_clf_dataset
     model = LogisticRegression(random_state=42, max_iter=200)
     model.fit(X, y)
     return model
 
 
 @pytest.fixture
-def lr_reg_model() -> LinearRegression:
+def lr_reg_model(background_reg_dataset) -> LinearRegression:
     """Return a simple linear regression model."""
-    X, y = make_regression(n_samples=100, n_features=7, random_state=42)
+    X, y = background_reg_dataset
     model = LinearRegression()
     model.fit(X, y)
     return model
 
 
 @pytest.fixture
-def lightgbm_reg_model():
+def lightgbm_reg_model(background_reg_dataset) -> Model:
     """Return a simple lightgbm regression model."""
     from lightgbm import LGBMRegressor
 
-    X, y = make_regression(n_samples=100, n_features=NR_FEATURES, random_state=42)
+    X, y = background_reg_dataset
     model = LGBMRegressor(random_state=42, n_estimators=3)
     model.fit(X, y)
     return model
 
 
 @pytest.fixture
-def lightgbm_clf_model():
+def lightgbm_clf_model(background_clf_dataset) -> Model:
     """Return a simple lightgbm classification model."""
     from lightgbm import LGBMClassifier
 
-    X, y = make_classification(
-        n_samples=100,
-        n_features=NR_FEATURES,
-        random_state=42,
-        n_classes=3,
-        n_informative=7,
-        n_repeated=0,
-        n_redundant=0,
-    )
+    X, y = background_clf_dataset
     model = LGBMClassifier(random_state=42, n_estimators=3)
     model.fit(X, y)
     return model
 
 
 @pytest.fixture
-def dt_clf_model_tree_model():
+def dt_clf_model_tree_model(background_clf_dataset) -> TreeModel:
     """Return a simple decision tree as a TreeModel."""
     from shapiq.explainer.tree.validation import validate_tree_model
 
-    X, y = make_classification(
-        n_samples=100,
-        n_features=NR_FEATURES,
-        random_state=42,
-        n_classes=3,
-        n_informative=7,
-        n_repeated=0,
-        n_redundant=0,
-    )
+    X, y = background_clf_dataset
     model = DecisionTreeClassifier(random_state=42, max_depth=3)
     model.fit(X, y)
     tree_model = validate_tree_model(model)
@@ -326,26 +260,18 @@ def dt_clf_model_tree_model():
 
 
 @pytest.fixture
-def rf_reg_model() -> RandomForestRegressor:
+def rf_reg_model(background_reg_dataset) -> RandomForestRegressor:
     """Return a simple random forest model."""
-    X, y = make_regression(n_samples=100, n_features=7, random_state=42)
+    X, y = background_reg_dataset
     model = RandomForestRegressor(random_state=42, max_depth=3, n_estimators=3)
     model.fit(X, y)
     return model
 
 
 @pytest.fixture
-def rf_clf_model() -> RandomForestClassifier:
+def rf_clf_model(background_clf_dataset) -> RandomForestClassifier:
     """Return a simple (classification) random forest model."""
-    X, y = make_classification(
-        n_samples=100,
-        n_features=NR_FEATURES,
-        random_state=42,
-        n_classes=3,
-        n_informative=7,
-        n_repeated=0,
-        n_redundant=0,
-    )
+    X, y = background_clf_dataset
     model = RandomForestClassifier(random_state=42, max_depth=3, n_estimators=3)
     model.fit(X, y)
     return model
@@ -356,8 +282,6 @@ def rf_clf_model() -> RandomForestClassifier:
 def if_clf_model(if_clf_dataset) -> IsolationForest:
     """Return a simple isolation forest model."""
     X, y = if_clf_dataset
-
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
     model = IsolationForest(random_state=42, n_estimators=3)
     model.fit(X, y)
     return model

tests/tests_explainer/tests_tree_explainer/test_tree_bugfix.py
@@ -1,8 +1,9 @@
 """This test module contains all tests for bugfixes regarding TreeSHAP-IQ."""
 
 import numpy as np
+import pytest
 
-from shapiq.explainer.tree import TreeModel, TreeSHAPIQ
+from shapiq.explainer.tree import TreeExplainer, TreeModel, TreeSHAPIQ
 
 
 def test_bike_bug():
@@ -241,3 +242,100 @@ def test_bike_bug():
     # if this test runs without an error, the bug is fixed
     assert True
     return
+
+
+def test_xgboost_bug():
+    """Test that xgboost works when not all features are used in the tree."""
+    import xgboost as xgb
+    from sklearn.datasets import make_regression
+
+    n_features_data = 7
+
+    # fit the tree on data that does not use all features
+    X, y = make_regression(random_state=42, n_samples=100, n_features=n_features_data)
+    model = xgb.XGBRegressor(random_state=42, n_estimators=10, max_depth=2)
+    model.fit(X, y)
+
+    # make sure not all features are used in the tree
+    booster = model.get_booster()
+    data_frame = booster.trees_to_dataframe()
+    feature_names = np.setdiff1d(data_frame["Feature"], "Leaf")
+    n_features_in_tree = len(feature_names)
+    assert booster.num_features() == n_features_data
+    assert booster.num_features != n_features_in_tree
+
+    # test the shapiq implementation
+    explainer = TreeExplainer(model=model, max_order=1, index="SV")
+    x_explain = X[0]
+    explanation = explainer.explain(x_explain)
+
+    for value in explanation.values:
+        assert not np.isnan(value)
+
+
+def test_xgb_predicts_with_wrong_leaf_node():
+    """This test illustrates that the predictions of the xgboost model do not perfectly align
+    with the xgboost models internal representation.
+
+    Sometimes the model goes in a wrong direction in the path. This test illustrates this where
+    one datapoint should be predicted with a left leave node but is predicted with the right leave
+    node with the xgboost model. We are parsing the xgboost model and creating our tree model
+    representation, where we correctly predict with the left leave node.
+    """
+
+    from sklearn.datasets import make_regression
+    from xgboost import XGBRegressor
+
+    X, y = make_regression(n_samples=100, n_features=7, random_state=42)
+    model = XGBRegressor(random_state=42, n_estimators=1, max_depth=1)
+    model.fit(X, y)
+    booster = model.get_booster()
+
+    # get the data point
+    x_explain = X[14]
+    x_explain_left = x_explain.copy()
+    x_explain_left[1] = 0.0  # set the feature value to 0.0 which is smaller than even the og. val
+    assert x_explain_left[1] < x_explain[1]  # make sure the value is smaller
+
+    # parse the xgboost model
+    df = booster.trees_to_dataframe()
+    threshold = df[df["Node"] == 0]["Split"].values[0]
+    feature_id = df[df["Node"] == 0]["Feature"].values[0]
+    intercept = model.intercept_[0]
+    prediction_left_df = df[df["Node"] == 1]["Gain"].values[0] + intercept
+    prediction_right_df = df[df["Node"] == 2]["Gain"].values[0] + intercept
+
+    # make sure the xgboost model is using the features we are playing around with
+    assert feature_id == "f1"  # feature 1 is used
+    assert x_explain[1] < threshold  # feature value is < threshold (this instance should go left)
+    assert len(df) == 3  # only 3 nodes in the tree (one decision node and two leaf nodes)
+
+    # get the predictions of the xgboost model
+    prediction_xgb = model.predict(x_explain.reshape(1, -1))
+    prediction_xgb_left = model.predict(x_explain_left.reshape(1, -1))
+    assert prediction_xgb != prediction_xgb_left  # predictions are different
+    # the original prediction is going right not left as it should
+    assert prediction_xgb == prediction_right_df
+    assert prediction_xgb_left == prediction_left_df
+
+    # get our tree model representation
+    tree_explainer = TreeExplainer(model=model, index="SV")
+    tree_model = tree_explainer._treeshapiq_explainers[0]._tree
+    prediction_tree_model = tree_model.predict_one(x_explain)
+    prediction_tree_model_left = tree_model.predict_one(x_explain_left)
+    # predictions of og xgb is different from our tree model
+    # where both instances are correctly predicted to be left
+    assert prediction_xgb != prediction_tree_model
+    assert prediction_tree_model == prediction_left_df
+    assert prediction_tree_model_left == prediction_left_df
+    assert prediction_tree_model != prediction_right_df
+
+    # get the explanation of the tree model
+    sv = tree_explainer.explain(x_explain)
+    efficiency = sum(sv.values)
+    if sv[()] == 0:
+        efficiency += sv.baseline_value
+    # efficiency is correct as the prediction with the tree model and not like the xgb model
+    assert pytest.approx(efficiency) == prediction_tree_model
+    assert pytest.approx(efficiency, rel=0.0001) == prediction_left_df
+    assert not pytest.approx(efficiency, rel=0.0001) == prediction_right_df

tests/tests_explainer/tests_tree_explainer/test_tree_explainer.py
@@ -176,7 +176,7 @@ def test_xgboost_reg(xgb_reg_model, background_reg_data):
 
 
 def test_xgboost_clf(xgb_clf_model, background_clf_data):
-    """Tests the shapiq implementation of TreeSHAP agains SHAP's implementation for XGBoost."""
+    """Tests the shapiq implementation of TreeSHAP against SHAP's implementation for XGBoost."""
 
     explanation_instance = 1
     class_label = 1
@@ -389,3 +389,57 @@ def test_iso_forest_shap(if_clf_model):
 
     assert baseline_shap == pytest.approx(baseline_shapiq, rel=1e-6)
     assert np.allclose(sv_shap, sv_shapiq_values, rtol=1e-5)
+
+
+def test_decision_stumps(background_reg_dataset, background_clf_dataset):
+    """Tests weather you can explain a decision stumps with the shapiq implementation."""
+    from lightgbm import LGBMClassifier, LGBMRegressor
+    from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+    from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+    from xgboost import XGBClassifier, XGBRegressor
+
+    models_reg = [
+        LGBMRegressor(random_state=42, n_estimators=20, max_depth=1),
+        XGBRegressor(random_state=42, n_estimators=20, max_depth=1),
+        DecisionTreeRegressor(random_state=42, max_depth=1),
+        RandomForestRegressor(random_state=42, n_estimators=20, max_depth=1),
+    ]
+
+    models_clf = [
+        LGBMClassifier(random_state=42, n_estimators=20, max_depth=1),
+        XGBClassifier(random_state=42, n_estimators=20, max_depth=1),
+        DecisionTreeClassifier(random_state=42, max_depth=1),
+        RandomForestClassifier(random_state=42, n_estimators=20, max_depth=1),
+    ]
+
+    for model in models_reg:
+        X, y = background_reg_dataset
+        model.fit(X, y)
+
+        explainer = TreeExplainer(model=model, max_order=3, index="k-SII")
+        x_explain = X[0]
+        explanation = explainer.explain(x_explain)
+
+        efficiency = sum(explanation.values)
+
+        pred = model.predict(x_explain.reshape(1, -1))
+        assert pred == pytest.approx(efficiency, rel=1e-5)
+
+    for model in models_clf:
+        X, y = background_clf_dataset
+        model.fit(X, y)
+
+        explainer = TreeExplainer(model=model, max_order=1, index="SV", class_index=0)
+        x_explain = X[1]
+        explanation = explainer.explain(x_explain)
+
+        efficiency = sum(explanation.values)
+
+        if isinstance(model, RandomForestClassifier) or isinstance(model, DecisionTreeClassifier):
+            pred = model.predict_proba(x_explain.reshape(1, -1))[0, 0]
+        elif isinstance(model, XGBClassifier):
+            pred = model.predict(x_explain.reshape(1, -1), output_margin=True)[0, 0]
+        else:  # skip lightgbm
+            continue
+
+        assert pred == pytest.approx(efficiency, rel=1e-5)