e3995cf7d544b23c3018760067abfc69a66f63e3
Maximilian <maximilian.muschalik@gmail.com>
2025-03-19 17:49:07 +0100

    updated version

--- shapiq/__init__.py
+++ shapiq/__init__.py
@@ -2,7 +2,7 @@
 the well established Shapley value and its generalization to interaction.
 """
 
-__version__ = "1.2.2"
+__version__ = "1.2.2.devopti"
 
 # approximator classes
 from .approximator import (
