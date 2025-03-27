e7cbfb64c34e6d77ba9e46d927e040c3c29e7f97
Justin Kang <justin.kang@alumni.ubc.ca>
2025-03-27 13:05:26 -0700

    Added SPEX tests, remove weights from SPEX

--- shapiq/approximator/sparse/spex.py
+++ shapiq/approximator/sparse/spex.py
@@ -17,8 +17,6 @@ class SPEX(Sparse):
            The Shapley interaction index type to use. Defaults to "FBII"
        top_order : bool, default=False
            If True, only reports interactions of order `max_order`.
-       sampling_weights : float, optional
-           Weights used for sampling.
        random_state : int, optional
            Seed for random number generator.
 
@@ -34,17 +32,15 @@ class SPEX(Sparse):
         index: str = "FBII",
         max_order: int | None = None,
         top_order: bool = False,
-        sampling_weights: float | None = None,
         random_state: int | None = None,
         decoder_type: str = "soft",
     ):
         super().__init__(
-            n,
-            max_order,
+            n=n,
+            max_order=max_order,
             index=index,
             top_order=top_order,
             random_state=random_state,
-            sampling_weights=sampling_weights,
             transform_type='fourier',
             decoder_type=decoder_type,
         )
