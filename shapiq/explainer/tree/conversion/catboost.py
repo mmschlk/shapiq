3b209476308e292876fe9844c0509d856612fd64
Isabell Hans <isabell@hans-online.de>
2025-04-25 16:15:02 +0200

    refactor code

--- shapiq/explainer/tree/conversion/catboost.py
+++ shapiq/explainer/tree/conversion/catboost.py
@@ -12,6 +12,7 @@ def convert_catboost(
     tree_model: Model,
 ) -> list[TreeModel]:
     """Transforms models from the catboost package to the format used by shapiq.
+     Note: part of this implementation is taken and adapted from the shap package, where it can be found in shap/explainers/_tree.py.
 
     Args:
         tree_model: The catboost model to convert.
@@ -22,7 +23,7 @@ def convert_catboost(
     """
     output_type = "raw"
 
-    # Logic from shap package
+    # workaround to obtain the single trees in the ensemble
     import json
     import tempfile
     from pathlib import Path
@@ -33,68 +34,60 @@ def convert_catboost(
         with Path.open(tmp_file, encoding="utf-8") as fh:
             loaded_cb_model = json.load(fh)
 
-    # load the CatBoost oblivious trees specific parameters
     num_trees = len(loaded_cb_model["oblivious_trees"])
 
     trees = []
     for tree_index in range(num_trees):
-        leaf_weights = loaded_cb_model["oblivious_trees"][tree_index]["leaf_weights"]
-        leaf_weights_unraveled = [0] * (len(leaf_weights) - 1) + leaf_weights
-        leaf_weights_unraveled[0] = sum(leaf_weights)
-        for index in range(len(leaf_weights) - 2, 0, -1):
-            leaf_weights_unraveled[index] = (
-                leaf_weights_unraveled[2 * index + 1] + leaf_weights_unraveled[2 * index + 2]
-            )
+        leaf_weights_json = loaded_cb_model["oblivious_trees"][tree_index]["leaf_weights"]
+        leaf_weights = [0] * (len(leaf_weights_json) - 1) + leaf_weights_json
+        leaf_weights[0] = sum(leaf_weights_json)
+        for index in range(len(leaf_weights_json) - 2, 0, -1):
+            leaf_weights[index] = leaf_weights[2 * index + 1] + leaf_weights[2 * index + 2]
 
-        leaf_values = loaded_cb_model["oblivious_trees"][tree_index]["leaf_values"]
-        leaf_values_unraveled = [0] * (len(leaf_values) - 1) + leaf_values
+        leaf_values_json = loaded_cb_model["oblivious_trees"][tree_index]["leaf_values"]
+        leaf_values = [0] * (len(leaf_values_json) - 1) + leaf_values_json
 
-        children_left = [i * 2 + 1 for i in range(len(leaf_values) - 1)]
-        children_left += [-1] * len(leaf_values)
+        children_left = [i * 2 + 1 for i in range(len(leaf_values_json) - 1)]
+        children_left += [-1] * len(leaf_values_json)
 
-        children_right = [i * 2 for i in range(1, len(leaf_values))]
-        children_right += [-1] * len(leaf_values)
-
-        children_default = [i * 2 + 1 for i in range(len(leaf_values) - 1)]
-        children_default += [-1] * len(leaf_values)
-
-        # load the split features and borders
-        # split features and borders go from leafs to the root
-        split_features_index = []
-        borders = []
+        children_right = [i * 2 for i in range(1, len(leaf_values_json))]
+        children_right += [-1] * len(leaf_values_json)
 
         # split features and borders go from leafs to the root
+        split_features_index_json = []
+        borders_json = []
+
         for elem in loaded_cb_model["oblivious_trees"][tree_index]["splits"]:
             split_type = elem.get("split_type")
             if split_type == "FloatFeature":
                 split_feature_index = elem.get("float_feature_index")
-                borders.append(elem["border"])
+                borders_json.append(elem["border"])
             elif split_type == "OneHotFeature":
                 split_feature_index = elem.get("cat_feature_index")
-                borders.append(elem["value"])
+                borders_json.append(elem["value"])
             else:
                 split_feature_index = elem.get("ctr_target_border_idx")
-                borders.append(elem["border"])
-            split_features_index.append(split_feature_index)
+                borders_json.append(elem["border"])
+            split_features_index_json.append(split_feature_index)
 
-        split_features_index_unraveled = []
-        for counter, feature_index in enumerate(split_features_index[::-1]):
-            split_features_index_unraveled += [feature_index] * (2**counter)
-        split_features_index_unraveled += [0] * len(leaf_values)
+        split_features_index = []
+        for counter, feature_index in enumerate(split_features_index_json[::-1]):
+            split_features_index += [feature_index] * (2**counter)
+        split_features_index += [0] * len(leaf_values_json)
 
-        borders_unraveled = []
-        for counter, border in enumerate(borders[::-1]):
-            borders_unraveled += [border] * (2**counter)
-        borders_unraveled += [0] * len(leaf_values)
+        borders = []
+        for counter, border in enumerate(borders_json[::-1]):
+            borders += [border] * (2**counter)
+        borders += [0] * len(leaf_values_json)
 
         trees.append(
             TreeModel(
                 children_left=np.array(children_left),
                 children_right=np.array(children_right),
-                features=np.array(split_features_index_unraveled),
-                thresholds=np.array(borders_unraveled),
-                values=np.array(leaf_values_unraveled).reshape((-1, 1)),
-                node_sample_weight=np.array(leaf_weights_unraveled),
+                features=np.array(split_features_index),
+                thresholds=np.array(borders),
+                values=np.array(leaf_values).reshape((-1, 1)),
+                node_sample_weight=np.array(leaf_weights),
                 empty_prediction=None,  # compute empty prediction later
                 original_output_type=output_type,
             )
