b5d773554c7e39c669c296ed447be9ddb67d2fed
Tobias Pitters <tobias.pitters@gmail.com>
2025-08-05 21:40:46 +0200

    fix pre-commit issues

--- tests/test_interaction_values.py
+++ tests/test_interaction_values.py
@@ -1,5 +1,9 @@
-from shapiq.interaction_values import InteractionValues
+from __future__ import annotations
+
 import numpy as np
+
+from shapiq.interaction_values import InteractionValues
+
 
 def test_interaction_values_eq():
     iv1 = InteractionValues(
@@ -10,9 +14,7 @@ def test_interaction_values_eq():
         estimation_budget=None,
         n_players=8,
         baseline_value=2.0719469373788755,
-        values=np.array(
-            [0.0] * 300
-        ),
+        values=np.array([0.0] * 300),
     )
     iv2 = InteractionValues(
         index="SV",
@@ -22,8 +24,6 @@ def test_interaction_values_eq():
         estimation_budget=None,
         n_players=8,
         baseline_value=2.071946937378876,  # Note the slight difference
-        values=np.array(
-            [0.0] * 300
-        ),
+        values=np.array([0.0] * 300),
     )
     assert iv1 == iv2
