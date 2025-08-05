a1057347de25806d3008a889dfd219c1d1775685
Tobias Pitters <tobias.pitters@gmail.com>
2025-08-05 21:37:58 +0200

    relax eq condition for interaction values

---
+++ tests/test_interaction_values.py
@@ -0,0 +1,29 @@
+from shapiq.interaction_values import InteractionValues
+import numpy as np
+
+def test_interaction_values_eq():
+    iv1 = InteractionValues(
+        index="SV",
+        max_order=1,
+        min_order=0,
+        estimated=False,
+        estimation_budget=None,
+        n_players=8,
+        baseline_value=2.0719469373788755,
+        values=np.array(
+            [0.0] * 300
+        ),
+    )
+    iv2 = InteractionValues(
+        index="SV",
+        max_order=1,
+        min_order=0,
+        estimated=False,
+        estimation_budget=None,
+        n_players=8,
+        baseline_value=2.071946937378876,  # Note the slight difference
+        values=np.array(
+            [0.0] * 300
+        ),
+    )
+    assert iv1 == iv2
