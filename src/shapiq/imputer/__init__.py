ed6acb3377e947a94ad1e1e0193f51de65220f8b
Hubert Baniecki <hbaniecki@gmail.com>
2025-11-26 14:07:59 +0100

    fix missing imports

--- src/shapiq/imputer/__init__.py
+++ src/shapiq/imputer/__init__.py
@@ -1,6 +1,7 @@
 """Imputer objects for the shapiq package."""
 
 from .baseline_imputer import BaselineImputer
+from .cte_imputer import CTEImputer
 from .gaussian_copula_imputer import GaussianCopulaImputer
 from .gaussian_imputer import GaussianImputer
 from .generative_conditional_imputer import GenerativeConditionalImputer
@@ -11,6 +12,7 @@ __all__ = [
     "MarginalImputer",
     "GenerativeConditionalImputer",
     "BaselineImputer",
+    "CTEImputer",
     "TabPFNImputer",
     "GaussianImputer",
     "GaussianCopulaImputer",
