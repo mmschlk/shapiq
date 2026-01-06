0f8fb6a154c9336e53fcb607fa19bc98e4472ad3
Mathis Weber <mathis.c.weber@web.de>
2026-01-06 11:29:10 +0100

    tests: test NN explainers with all train points instead of just 1

--- tests/shapiq/tests_unit/tests_explainer/tests_nn_explainers/tests_tnn_explainer.py
+++ tests/shapiq/tests_unit/tests_explainer/tests_nn_explainers/tests_tnn_explainer.py
@@ -10,16 +10,15 @@ def test_tnn(sklearn_tnn_model, background_clf_dataset_small):
     X, y = background_clf_dataset_small
     n_classes = np.max(y) + 1
 
-    x_test = X[0]
+    for x_test in X:
+        for class_index in range(n_classes):
+            ground_truth_game = TNNExplainerXAI(sklearn_tnn_model, x_test, class_index)
+            iv_expected = ground_truth_game.exact_values("SV", 1)
+            knn_explainer = ThresholdNNExplainer(sklearn_tnn_model, class_index=class_index)
+            iv = knn_explainer.explain(x_test)
 
-    for class_index in range(n_classes):
-        ground_truth_game = TNNExplainerXAI(sklearn_tnn_model, x_test, class_index)
-        iv_expected = ground_truth_game.exact_values("SV", 1)
-        knn_explainer = ThresholdNNExplainer(sklearn_tnn_model, class_index=class_index)
-        iv = knn_explainer.explain(x_test)
+            interactions = iv.interactions.keys()
+            iv_expected_array = np.array([iv_expected.interactions[ia] for ia in interactions])
+            iv_array = np.array([iv.interactions[ia] for ia in interactions])
 
-        interactions = iv.interactions.keys()
-        iv_expected_array = np.array([iv_expected.interactions[ia] for ia in interactions])
-        iv_array = np.array([iv.interactions[ia] for ia in interactions])
-
-        assert np.allclose(iv_expected_array, iv_array)
+            assert np.allclose(iv_expected_array, iv_array)
--- tests/shapiq/tests_unit/tests_explainer/tests_nn_explainers/tests_wknn_explainer.py
+++ tests/shapiq/tests_unit/tests_explainer/tests_nn_explainers/tests_wknn_explainer.py
@@ -11,20 +11,19 @@ def test_wknn(sklearn_wknn_model, background_clf_dataset_small):
     n_classes = np.max(y) + 1
     n_bits = 3
 
-    x_test = X[0]
+    for x_test in X:
+        for class_index in range(n_classes):
+            ground_truth_game = WeightedKNNExplainerXAI(
+                sklearn_wknn_model, x_test, class_index, n_bits=n_bits
+            )
+            iv_expected = ground_truth_game.exact_values("SV", 1)
+            knn_explainer = WeightedKNNExplainer(
+                sklearn_wknn_model, class_index=class_index, n_bits=n_bits
+            )
+            iv = knn_explainer.explain(x_test)
 
-    for class_index in range(n_classes):
-        ground_truth_game = WeightedKNNExplainerXAI(
-            sklearn_wknn_model, x_test, class_index, n_bits=n_bits
-        )
-        iv_expected = ground_truth_game.exact_values("SV", 1)
-        knn_explainer = WeightedKNNExplainer(
-            sklearn_wknn_model, class_index=class_index, n_bits=n_bits
-        )
-        iv = knn_explainer.explain(x_test)
+            interactions = iv.interactions.keys()
+            iv_expected_array = np.array([iv_expected.interactions[ia] for ia in interactions])
+            iv_array = np.array([iv.interactions[ia] for ia in interactions])
 
-        interactions = iv.interactions.keys()
-        iv_expected_array = np.array([iv_expected.interactions[ia] for ia in interactions])
-        iv_array = np.array([iv.interactions[ia] for ia in interactions])
-
-        assert np.allclose(iv_expected_array, iv_array)
+            assert np.allclose(iv_expected_array, iv_array)
