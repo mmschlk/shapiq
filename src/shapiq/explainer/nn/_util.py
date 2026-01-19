3db23d1a347aa1f97dad01792664513202e4db78
Mathis Weber <mathis.c.weber@web.de>
2026-01-19 19:01:54 +0100

    chore: delete duplicate function

--- src/shapiq/explainer/nn/_util.py
+++ src/shapiq/explainer/nn/_util.py
@@ -5,39 +5,12 @@ from __future__ import annotations
 import logging
 from typing import TYPE_CHECKING, Any
 
-import numpy as np
-
 if TYPE_CHECKING:
     from collections.abc import Iterable, Mapping
-
-    import numpy.typing as npt
 
     from shapiq.explainer.custom_types import ExplainerIndices
 
 logger = logging.getLogger()
-
-
-def keep_first_n(mask: npt.NDArray[np.bool], n: int) -> npt.NDArray[np.bool]:
-    """Sets all entries of the input array to False except the first ``n`` entries with value ``True``.
-
-    This will just return a reference to the input array if ``np.sum(mask) <= n``
-
-    Args:
-        mask: The mask in question.
-        n: The maximum number of true entries.
-    """
-    if n == 0:
-        return np.zeros_like(mask)
-
-    n_true = 0
-    for i, val in enumerate(mask):
-        n_true += int(val)
-        if n_true == n:
-            out = np.zeros_like(mask)
-            out[: i + 1] = mask[: i + 1]
-            return out
-
-    return mask
 
 
 def warn_ignored_parameters(
