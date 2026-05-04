ddc4c90f64dcda003053a11b1ae55151516da820
wittmannu <Ulrich.Wittmann@campus.lmu.de>
2026-05-04 16:01:42 +0200

    type annotations for increased clarity of code

--- src/demo/approximator_testing.py
+++ src/demo/approximator_testing.py
@@ -3,12 +3,37 @@ from shapiq.game import Game
 from shapiq_games.synthetic import DummyGame
 from shapiq_games.synthetic import RandomGame
 from shapiq_games.synthetic import SOUM
-from shapiq.approximator import ProxySHAP
+from shapiq.approximator import ProxySHAP, Approximator
 from shapiq import ExactComputer #used to compute GT
+from typing import Literal
+
+InteractionIndex = Literal[
+    "SV",
+    "BV",
+    "SII",
+    "BII",
+    "k-SII",
+    "STII",
+    "FBII",
+    "FSII",
+    "kADD-SHAP",
+    "CHII",
+]
 import numpy as np
 
-def single_run(game, index, max_order, approximator, budget, seed):
-    approx_values = approximator.approximate(budget, game)
+def single_run(
+        game: Game,
+        index: InteractionIndex,
+        max_order: int,
+        approximator_class: type[Approximator],
+        budget: int,
+        seed: int,
+) -> None:
+    approximator: Approximator = approximator_class(n=game.n_players,
+                                      index=index,
+                                      max_order=max_order,
+                                      random_state=seed)
+    approx_values: InteractionValues = approximator.approximate(budget, game)
     #approx_values.plot_upset()
     exact = ExactComputer(game=game)
     ground_truth = exact.__call__(index=index, order=max_order)
@@ -41,11 +66,13 @@ def demo():
     seed = 42
     max_order = 2
     # select index (certain indices like SV expect specific order(1)! )
+    # We probably also need to check which approximator supports which index.
     index = "SII"
     game = SOUM(n=10, n_basis_games=20, min_interaction_size=1,
                 max_interaction_size=max_order, random_state=seed)
-    approximator = ProxySHAP(n=game.n_players, index=index, max_order=max_order)
+    #We only provide the approximator class to build approximators with different seeds
+    approximator_class = ProxySHAP
     budget = 100
-    single_run(game, index, max_order, approximator, budget, seed)
+    single_run(game, index, max_order, approximator_class, budget, seed)
 
 demo()
