2f0f3b2c5065f0fedda336eec36906b78aff63e8
wittmannu <Ulrich.Wittmann@campus.lmu.de>
2026-05-04 17:22:34 +0200

    cleanup for single run demo

--- src/demo/demo.py
+++ src/demo/demo.py
@@ -2,31 +2,52 @@ from shapiq_games.synthetic import SOUM
 from shapiq.approximator import ProxySHAP, Approximator
 from custom_types import InteractionIndex, MetricFunction
 from metrics import mse_metric, mae_metric
-from runner import single_run
+from runner import approximate, compute_ground_truth, compute_metrics
 
 def demo() -> None:
     print("This is a test to see how game approximation works")
 
     # Define the values
-    seed = 42
+    game_seed = 42
+    approx_seed = 42
     max_order = 2
     # select index (certain indices like SV expect specific order(1)! )
     # We probably also need to check which approximator supports which index.
     index : InteractionIndex = "SII"
     game = SOUM(n=10, n_basis_games=20, min_interaction_size=1,
-                max_interaction_size=max_order, random_state=seed)
+                max_interaction_size=max_order, random_state=game_seed)
     #We only provide the approximator class to build approximators with different seeds
     approximator_class: type[Approximator] = ProxySHAP
     budget = 100
-
     #metrics are just functions for now, this should be refined into a clean easy to work with structure
     #maybe build a metric object
     metrics: dict[str, MetricFunction] = {
         "mse": mse_metric,
         "mae": mae_metric,
     }
-    metric_results: dict[str, float] \
-        = single_run(game, index, max_order, approximator_class, budget, seed, metrics)
+
+
+    #Compute ground truth
+    ground_truth = compute_ground_truth(game=game, index=index, max_order=max_order)
+
+    #approximate values [later: n times]
+    approx_values = approximate(
+        game=game,
+        approximator_class=approximator_class,
+        index=index,
+        max_order=max_order,
+        budget=budget,
+        seed=approx_seed
+    )
+
+    #calculate metrics
+    metric_results: dict[str, float] = compute_metrics(
+        ground_truth=ground_truth,
+        approximation=approx_values,
+        metrics=metrics
+    )
+
+    #missing: aggregation of values over multiple runs
 
     print("Metric results:")
     for metric_name, metric_value in metric_results.items():
--- src/demo/runner.py
+++ src/demo/runner.py
@@ -3,31 +3,47 @@ from shapiq.approximator import Approximator
 from shapiq.game import Game
 from custom_types import InteractionIndex, MetricFunction
 
-def single_run(
+
+
+def compute_ground_truth(
+    game: Game,
+    index: InteractionIndex,
+    max_order: int,
+) -> InteractionValues:
+    exact = ExactComputer(game=game)
+    return exact(index=index, order=max_order)
+    #ground_truth.plot_upset()
+
+
+def approximate(
         game: Game,
         index: InteractionIndex,
         max_order: int,
         approximator_class: type[Approximator],
         budget: int,
-        seed: int,
-        metrics: dict[str, MetricFunction]
-) -> dict[str, float]:
+        seed: int
+) -> InteractionValues:
     approximator: Approximator = approximator_class(n=game.n_players,
                                       index=index,
                                       max_order=max_order,
                                       random_state=seed)
-    approx_values: InteractionValues = approximator.approximate(budget, game)
+    approx_values = approximator.approximate(budget, game)
     #approx_values.plot_upset()
-    exact = ExactComputer(game=game)
-    ground_truth: InteractionValues = exact.__call__(index=index, order=max_order)
-    #ground_truth.plot_upset()
+    return approx_values
 
-    metric_results = {}
+
+
+def compute_metrics(
+    ground_truth: InteractionValues,
+    approximation: InteractionValues,
+    metrics: dict[str, MetricFunction],
+) -> dict[str, float]:
+    metric_results: dict[str, float] = {}
 
     for metric_name, metric_func in metrics.items():
         metric_results[metric_name] = metric_func(
             ground_truth,
-            approx_values,
+            approximation,
         )
 
     return metric_results
