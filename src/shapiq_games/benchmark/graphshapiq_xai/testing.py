448a849e45f50246ef38f6ba1563e5ff9e0565c4
Finn Jacob Eliyah Krammer <finn.krammer@check24.de>
2026-05-05 17:24:05 +0200

    commented out the print statements to satisfy linting

--- src/shapiq_games/benchmark/graphshapiq_xai/testing.py
+++ src/shapiq_games/benchmark/graphshapiq_xai/testing.py
@@ -2,16 +2,12 @@
 
 from __future__ import annotations
 
-import logging
-
 import numpy as np
 import torch
 from torch_geometric.data import Data
 
 from shapiq_games.benchmark.graphshapiq_xai.base import GraphGame
 from shapiq_games.benchmark.graphshapiq_xai.test_models import GCN2Layer
-
-logger = logging.getLogger()
 
 x = torch.tensor([[1.0, 5.0, 3.0], [4.0, 2.0, 6.0]], dtype=torch.float)
 
@@ -34,9 +30,12 @@ game = GraphGame(
 coalition = np.array([1, 0])
 masked_graph = game.mask_input(coalition)
 
-logger.log("Original Graph Features")
-logger.log(x_graph.x, "\n")
-logger.log("Coalition (Mask Array)")
-logger.log(coalition, "\n")
-logger.log("Masked Graph Features")
-logger.log(masked_graph.x)
+# Uncomment. Linting doesn't let me push this.
+"""
+print("Original Graph Features")
+print(x_graph.x, "\n")
+print("Coalition (Mask Array)")
+print(coalition, "\n")
+print("Masked Graph Features")
+print(masked_graph.x)
+"""
